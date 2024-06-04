72a06a27a0896e5dee88710b56ca1232a5ad47b0
Dmitrii <d.bargan@innopolis.university>
2024-06-04 18:32:07 +0300

    Copy the template over to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,91 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const Lab1());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Lab1 extends StatelessWidget {
+  const Lab1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: LAb1HomePage(),
+    );
+  }
+}
+
+class LAb1HomePage extends StatelessWidget {
+  const LAb1HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: const Icon(Icons.stars),
+        title: const Text('Lab 2'),
+        backgroundColor: Colors.teal,
+        elevation: 4,
       ),
+      body: myWidget(),
+    );
+  }
+
+  Widget myWidget() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 25,
+          ),
+          exercise1(),
+          const SizedBox(
+            height: 25,
+          ),
+          exercise2(),
+          const SizedBox(
+            height: 25,
+          ),
+          exercise3(),
+          const SizedBox(
+            height: 25,
+          ),
+          exercise4(),
+          const SizedBox(
+            height: 25,
+          ),
+          exercise5(),
+        ],
+      ),
+    );
+  }
+
+  Widget exercise1() {
+    return const FlutterLogo(
+      size: 100,
+    );
+  }
+
+  Widget exercise2() {
+    return const FlutterLogo(
+      size: 100,
+    );
+  }
+
+  Widget exercise3() {
+    return const FlutterLogo(
+      size: 100,
+    );
+  }
+
+  Widget exercise4() {
+    return const FlutterLogo(
+      size: 100,
+    );
+  }
+
+  Widget exercise5() {
+    return const FlutterLogo(
+      size: 100,
     );
   }
 }
