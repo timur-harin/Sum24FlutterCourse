046dbe64d15a5d473259c359b1dfe942bce53514
Gurbanberdi Gulladyyev <g.gulladyyev@innopolis.university>
2024-06-04 20:51:19 +0300

    lab1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Lab1());
 
@@ -60,32 +62,59 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return const Text("Hello Flutter!",
+        style: TextStyle(
+            fontSize: 100, color: Colors.cyan, fontWeight: FontWeight.w900));
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
+    return const Icon(
+      Icons.adjust_sharp,
       size: 100,
+      color: Colors.orange,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
+    Image image = Image.network(
+      "https://www.positive.news/wp-content/uploads/2022/03/GOLD-%C2%A9-Sam-Wilson-min.jpg",
+      width: 100,
+      height: 100,
+      fit: BoxFit.cover,
     );
+
+    return image;
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    TextButton button = TextButton(
+        onPressed: () {
+          print('You pressed the button');
+        },
+        child: const Text('Pressed'));
+    return button;
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    Column column = Column(
+      children: [
+        Container(
+            child: Text("This is a text"),
+            width: 250,
+            height: 50,
+            padding: EdgeInsets.all(16),
+            decoration:
+                BoxDecoration(border: Border.all(color: Colors.black26))),
+        Container(
+          child: Icon(Icons.access_alarm),
+          width: 250,
+          height: 50,
+          padding: EdgeInsets.only(top: 10),
+          decoration: BoxDecoration(
+              color: Colors.brown, border: Border.all(color: Colors.black26)),
+        )
+      ],
     );
+    return column;
   }
 }
