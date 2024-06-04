1cea31cb0013c88dac0e58702ca1b31cfef8dca3
alexg <alex.gorbanev05@gmail.com>
2024-06-04 19:03:07 +0500

    Finished lab1

--- lib/templates/lab1.dart
+++ lib/templates/lab1.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Lab1());
 
@@ -60,32 +62,69 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text(
+      "Hello Flutter!",
+      style: TextStyle(
+        fontSize: 52,
+        color: Colors.redAccent,
+        fontWeight: FontWeight.bold
+      ),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.home,
+      size: 52,
+      color: Colors.blue,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
+    return Container(
+      width: 200,
+      height: 200,
+      child: Image.network(
+      'https://loginportal9999.funnyjunk.com/pictures/Bober_f70283_11166797.jpg',
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      onPressed: () {
+        print('Pressed');
+      },
+      child: Text('Press me'),
     );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(5),
+          padding: EdgeInsets.all(7),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.green, width: 3),
+            color: Colors.yellow,
+          ),
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 2),
+            color: Colors.blue,
+          ),
+          child: Icon(Icons.abc),
+        ),
+      ],
     );
   }
 }
