d11c4cfdfdfde245443a95970211e48eee385000
Resent <remal11151@gmail.com>
2024-06-04 19:08:52 +0300

    lab complete

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -60,32 +60,49 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text(
+      "Hello, flutter!",
+      style: TextStyle(
+        fontSize: 40,
+        color: Colors.red,
+        fontWeight: FontWeight.bold
+      ),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.favorite,
+      size: 40,
+      color: Colors.blue,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return const Image(
+        image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw9YtRKVz9bKUSiwFgMt5COEU-FnTaPeUW3g&s'),
+      );
+  }
+
+  onPressed(){
+    print("Pressed");
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return TextButton(onPressed: onPressed, child: const Text("Press me"));
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return  Column(
+      children: [
+        Container(padding: const EdgeInsets.all(8.0),
+                  color: Colors.blue[600],
+                  alignment: Alignment.center,
+                  child : const Text("text here")),
+        Container(alignment: Alignment.bottomLeft,
+                  child: const Icon(Icons.sim_card_download, size: 60))
+       ],
+      
     );
   }
 }
