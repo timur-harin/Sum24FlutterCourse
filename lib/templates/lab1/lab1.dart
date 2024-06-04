7d54ff5fcc1ac1b8bf88855e1dc347f49da50da5
wixlyx <petrova.kate-8530@yandex.ru>
2024-06-04 20:31:57 +0300

    init lab 1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Lab1());
 
@@ -22,8 +24,8 @@ class LAb1HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: const Icon(Icons.stars),
-        title: const Text('Lab 2'),
-        backgroundColor: Colors.teal,
+        title: const Text('Lab 1'),
+        backgroundColor: Colors.indigo,
         elevation: 4,
       ),
       body: myWidget(),
@@ -60,32 +62,66 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return Text("Hello Flutter!", 
+      style: TextStyle(
+      fontSize: 30, 
+      color: Colors.indigo, 
+      fontWeight: FontWeight.bold
+      ),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return Icon(Icons.people_sharp, size: 100,color: Colors.indigo);
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
-    );
+     return Image.network("https://img.joinfo.com/i/2020/03/800x0/5e725ed206a20.jpg", height: 1000, width: 500, );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return TextButton(
+      onPressed: () {
+        print("Clicked!");
+      },
+      child: Container(
+        color: Colors.indigo,
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        child: const Text("Press", style: TextStyle(color: Colors.white)),
+      ),
+      );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
-}
+  return Column(
+    children: [
+      Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.indigo,
+          border: Border.all(color: Colors.black, width: 2),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Text(
+          'Some text widget below a kitten',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+      Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.indigo,
+          border: Border.all(color: Colors.black, width: 2),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Icon(
+          Icons.landscape,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+    ],
+  );
+  }}
