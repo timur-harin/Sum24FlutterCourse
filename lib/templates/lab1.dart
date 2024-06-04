82afbf917eb47b22a68adf7eb904ab29166523b9
Карим Закиров <karimzakirov05@mail.ru>
2024-06-04 20:54:07 +0300

    Karim Zakirov

--- lib/templates/lab1.dart
+++ lib/templates/lab1.dart
@@ -60,32 +60,68 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+
+    return const Text(
+      "Hello Flutter!",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+      
+    
     );
+
   }
+    
+  
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.home,
+      size: 30,
+      color: Colors.purple,
     );
   }
 
-  Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
+ Widget exercise3() {
+  return Image.network(
+    'https://sportishka.com/uploads/posts/2022-11/1667502411_55-sportishka-com-p-nissan-mini-kuper-instagram-64.jpg',
+    width: 100, 
+    height: 50,
+    
+  );
+}
+
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      child: const Text("Press_me"),
+      onPressed: (){
+          print("Pressed");
+      },
     );
   }
 
-  Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
-    );
-  }
+ Widget exercise5() {
+  return Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Text(
+          "text",
+          style: TextStyle(color: Colors.blue),
+        ),
+      ),
+      SizedBox(width: 20), 
+      Container(
+        child: const Icon(Icons.cloud),
+          padding: const EdgeInsets.all(30.0),
+          decoration: ShapeDecoration(shape: Border.all(color: Colors.yellow, width: 20)),
+      ),
+    ],
+  );
 }
+
+}
+
