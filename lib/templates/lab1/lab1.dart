60e7f3d9fa22d8e8070db9adfc682e493e10fae7
Стася Малахова <stasiamalakhova@MacBook-Air-Stasa.local>
2024-06-04 20:35:59 +0300

    initil lab1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Lab1());
 
@@ -60,32 +61,57 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
-    );
+    return const Text('Hello Flutter!',
+        style: TextStyle(
+          fontSize: 50,
+          color: Colors.pinkAccent,
+          fontWeight: FontWeight.bold,
+        ));
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
+    return const Icon(
+      Icons.money,
       size: 100,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
+    return Image.network(
+      'https://avatars.dzeninfra.ru/get-zen_doc/1328418/pub_5d865696a3f6e400ad809169_5d8656e432335400af4cf357/scale_1200',
+      width: 200,
+      height: 200,
+      fit: BoxFit.fill,
     );
   }
 
+  void tap() {
+    print('Print');
+  }
+
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      onPressed: tap,
+      child: Text('Tap'),
     );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Text(
+          'test',
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.pinkAccent,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          alignment: Alignment.bottomLeft,
+          child: Icon(Icons.home),
+        ),
+      ],
     );
   }
 }
