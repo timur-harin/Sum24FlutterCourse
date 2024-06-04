bfc75e089a6d2bbbeed6748b707ba8532eb74d23
Klimentii Chistyakov <klimspy@gmail.com>
2024-06-04 20:23:57 +0700

    Lab 1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Lab1());
 
@@ -22,7 +24,7 @@ class LAb1HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: const Icon(Icons.stars),
-        title: const Text('Lab 2'),
+        title: const Text('Lab 1'),
         backgroundColor: Colors.teal,
         elevation: 4,
       ),
@@ -60,32 +62,95 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text("Hello Flutter!",
+      style: TextStyle(
+        color: Colors.cyan,
+        fontSize: 100,
+        fontWeight: FontWeight.normal
+      ),
+
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.account_box_outlined,
+      size: 50,
+      color: Colors.pink,
+      weight: 1,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
+    return const Image(
+      image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqJxPznJUf7ginHrFwVaxHPJ4TXxaeepJu6A&s"),
+      width: 750,
+      height: 925,
+      fit: BoxFit.cover,
     );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      onPressed: onPressedButtonOne,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.pink
+      ),
+      child: textForButton(),
+    );
+  }
+
+  void onPressedButtonOne() {
+    print("Why do you press buttons, are you what, a button presser???");
+  }
+
+  Widget textForButton() {
+    return const Text(
+      "Press me",
+      style: TextStyle(
+        color: Colors.yellow,
+        fontSize: 30,
+      ),
     );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.cyan,
+              width: 3
+            )
+          ),
+          child: textForExercise5()
+        ),
+        Container(
+          padding: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.pink,
+              width: 10
+            )
+          ),
+          child: const Icon(
+            Icons.star_border_outlined,
+            color: Colors.pink,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget textForExercise5() {
+    return const Text(
+      "Some text",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.purple
+      ),
     );
   }
 }
