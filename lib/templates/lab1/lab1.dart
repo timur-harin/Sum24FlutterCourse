0e165546d2a0ab22777885f69ca0f3fcdaec6d62
GG_Host <gd.host@yandex.ru>
2024-06-04 19:37:53 +0300

    lab1 Antipov Dmitrii ISE01

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -60,33 +60,58 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return Text(
+    return const Text(
       "Hello Flutter!",
       style: TextStyle(
-          fontSize: 100, color: Colors.red, fontWeight: FontWeight.bold),
+          color: Color.fromARGB(255, 255, 0, 0),
+          fontSize: 40,
+          fontWeight: FontWeight.bold),
     );
   }
 
   Widget exercise2() {
-    return Icon(Icons.home, size: 100, color: Colors.blue);
+    return const Icon(Icons.home,
+        size: 200, color: Color.fromARGB(255, 0, 255, 0));
   }
 
   Widget exercise3() {
     return Image.network(
-        "https://www.w-dog.ru/wallpapers/10/1/500321873866125/priroda-les-trava.jpg");
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Wiki_Test_Image.jpg/800px-Wiki_Test_Image.jpg",
+        width: 100,
+        height: 100,
+        fit: BoxFit.cover);
   }
 
   Widget exercise4() {
     return TextButton(
-        onPressed: () {
-          print('tapped');
-        },
-        child: const Text('Tap me!'));
+        onPressed: () => (print("Pressed")), child: const Text("press it"));
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsetsDirectional.symmetric(
+              horizontal: 10, vertical: 30),
+          margin: const EdgeInsets.symmetric(vertical: 50),
+          decoration: BoxDecoration(
+              color: Color.fromARGB(0, 32, 72, 87),
+              border: Border.all(color: Color.fromARGB(255, 100, 100, 100))),
+          child: const Text(
+            "Column",
+          ),
+        ),
+        Container(
+          padding: const EdgeInsetsDirectional.symmetric(
+              horizontal: 10, vertical: 30),
+          margin: const EdgeInsets.symmetric(vertical: 50),
+          decoration: const BoxDecoration(color: Color.fromARGB(0, 32, 72, 87)),
+          child: Image.network(
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Wiki_Test_Image.jpg/800px-Wiki_Test_Image.jpg",
+              width: 100,
+              height: 100),
+        ),
+      ],
     );
   }
 }
--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,76 @@
-class Book {
-  // your code here
+class Book with ConvertibleToJson {
+  String title;
+  String author;
+  int pageCount;
+
+  Book(this.title, this.author, this.pageCount);
+
+  void displayBookInfo() {
+    print('Title: $title, Author: $author, Page Count: $pageCount');
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {'title': title, 'author': author, 'pageCount': pageCount};
+  }
 }
 
-class FictionBook {
-  // your code here
+class Novel extends Book {
+  String category;
+
+  Novel(String title, String author, int pageCount, this.category)
+      : super(title, author, pageCount);
+
+  @override
+  void displayBookInfo() {
+    print(
+        'Title: $title, Author: $author, Page Count: $pageCount, Genre: $category');
+  }
 }
 
-mixin JsonSerializable {
-  // your code here
+mixin ConvertibleToJson {
+  Map<String, dynamic> toJson();
 }
 
-double calculateArea(){
-  // your code here
+double computeArea(
+    {required double length,
+    required double width,
+    String shape = 'rectangle'}) {
+  const double pi = 3.14;
+  if (shape == 'circle') {
+    return pi * length * length;
+  } else {
+    return length * width;
+  }
 }
 
-class User{
-  // your code here
+class User {
+  String name;
+  int? age;
+  String? email;
+
+  User({required this.name, this.age, this.email});
+
+  void showUserInfo() {
+    String ageInfo = age?.toString() ?? 'N/A';
+    String emailInfo = email ?? 'N/A';
+    print('Name: $name');
+    print('Age: $ageInfo');
+    print('Email: $emailInfo');
+  }
+}
+
+void main() {
+  Book myBook = Book('Brave New World', 'Aldous Huxley', 311);
+  myBook.displayBookInfo();
+  print(myBook.toJson());
+
+  Novel myNovel = Novel('The Hobbit', 'J.R.R. Tolkien', 310, 'Fantasy');
+  myNovel.displayBookInfo();
+
+  print(computeArea(length: 7.0, width: 14.0)); // Rectangle area
+  print(computeArea(length: 7.0, width: 0, shape: 'circle')); // Circle area
+
+  User newUser = User(name: 'Bob', age: null, email: 'bob@example.com');
+  newUser.showUserInfo();
 }
