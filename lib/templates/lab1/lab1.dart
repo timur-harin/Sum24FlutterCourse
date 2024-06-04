20323492100faac75ead564d796ce68a583f9ce9
Mikhail Trifonov <trifonov2812@gmail.com>
2024-06-04 20:29:42 +0300

    lab1 solutions

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -60,32 +60,75 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text(
+      "Hello, world!",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.green,
+        fontWeight: FontWeight.bold
+      )
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
-      size: 100,
+    return const Icon(
+      Icons.phone_android_rounded,
+      size: 50,
+      color: Colors.purple,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
+    return const Image(
+      image: NetworkImage("https://steamuserimages-a.akamaihd.net/ugc/931558048644462852/43896ACB32A364E4F86855909BAEA92CD7136F48/?imw=512&amp;imh=288&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true"),
+      width: 400,
+      height: 200,
+      fit: BoxFit.fitHeight,
     );
   }
 
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      onPressed: onPressed,
+      child: const Text("the кнопкааа"),
     );
   }
 
+  void onPressed() {
+    print("Pressed");
+  }
+
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Padding(
+            padding: const EdgeInsets.all(10),
+            child: 
+              Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      width: 5,
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: const Text(
+                    "text"
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  child: const Icon(
+                    Icons.anchor_rounded,
+                    size: 50,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ],
+            )
     );
   }
 }
