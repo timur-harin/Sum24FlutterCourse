944532d917c38f068cea9cb9322f25cf1c05d25c
mc_lavrushka <mc.lavrushka@gmail.com>
2024-06-05 19:28:50 +0300

    lab2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:education/templates/lab2/lab2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:group_button/group_button.dart';
 
 void main() {
@@ -10,12 +12,95 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+          body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  Book book = Book("Harry Potter", "JK Rowling", 100);
+                  book.bookDetails();
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.question_mark_rounded),
+                    Text("Book Details")
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  FictionBook book =
+                      FictionBook("Pirates of Carribean", "J. K. Rowling", 100, "Fiction");
+                  print(book.toJson());
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.question_mark_rounded),
+                    Text("Fiction Book Details")
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  Book book = Book("Alice", "Lewis Carroll", 1030);
+                  print(book.toJson());
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.question_mark_rounded),
+                    Text("Book toJson")
+                  ],
+                ),
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                  print(calculateArea(length: 100, width: 100));
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.question_mark_rounded),
+                    Text("Calculate Area")
+                  ],
+                ),
+              ),
+            ),
+             Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              child: InkWell(
+                onTap: () {
+                 User user = User("Name", 10, null);
+                 user.printUserDetails();
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.question_mark_rounded),
+                    Text("User null-safety")
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,74 @@
-class Book {
-  // your code here
+import 'package:flutter/material.dart';
+
+class Book with JsonSerializable {
+  String title;
+  String author;
+  int numberOfPages;
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print("title: $title , author: $author , number of pages: $numberOfPages");
+  }
+
+  // Implement the toJson method from the mixin
+  @override
+  Map<String, dynamic> toJson() {
+    var map = {
+      "title": title,
+      "author": author,
+      "numberOfPages": numberOfPages
+    };
+    return map;
+  }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book with JsonSerializable {
+  String genre;
+  FictionBook(String title, String author, int numberOfPages, this.genre)
+      : super(title, author, numberOfPages);
+  @override
+  void bookDetails() {
+    print(
+        "title: $title , author: $author , number of pages: $numberOfPages , genre: $genre");
+  }
 }
 
 mixin JsonSerializable {
-  // your code here
+  String get title;
+  String get author;
+  int get numberOfPages;
+
+  Map<String, dynamic> toJson() {
+    var map = {
+      "title": title,
+      "author": author,
+      "numberOfPages": numberOfPages
+    };
+    return map;
+  }
 }
 
-double calculateArea(){
-  // your code here
+double calculateArea(
+    {required double length,
+    required double width,
+    String shape = "rectangle"}) {
+  if (shape == "circle") {
+    return 3.14 * length * length;
+  }
+  return length * width;
 }
 
-class User{
+class User {
+  String name;
+  int? age;
+  String? email;
+
+  User(this.name, this.age, this.email);
+
+  void printUserDetails() {
+    print("Name: $name ");
+    print("Age: ${age ?? "N/A"} ");
+    print("Email: ${email?? "N/A"} ");
+  }
   // your code here
 }
