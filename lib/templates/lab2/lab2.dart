7982bd5d2721f06b79a6e3a6ec25b32ac52f72b1
wixlyx <petrova.kate-8530@yandex.ru>
2024-06-05 20:03:02 +0300

    lab 2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,57 @@
-class Book {
-  // your code here
+class Book implements JsonSerializable{
+   String title;
+   String author;
+   int numberOfPages;
+   Book(this.title, this.author, this.numberOfPages);
+
+   void bookDetails(){
+    print('Title: $title, Author: $author, Number of Pages: $numberOfPages');
+   }
+
+   @override
+   Map<String, dynamic> toJson(){
+    return{
+      "title": title,
+      "author" : author, 
+      'numberOfPages': numberOfPages,
+    };
+   }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+   String genre;
+
+   FictionBook(String title, String author, int numberOfPages, this.genre) : super(title, author, numberOfPages);
+
+   @override
+   void bookDetails(){
+    print('Title: $title, Author: $author, Genre: $genre,  Number of Pages: $numberOfPages');
+   }
 }
 
 mixin JsonSerializable {
-  // your code here
+  Map<String, dynamic> toJson();
 }
 
-double calculateArea(){
-  // your code here
+double calculateArea({required length, required width, String shape='rectangle'}){
+  if (shape == 'circle') {
+    return 3.14 * length * length; 
+  }
+  return length * width;
 }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+  User({required this.name, this.age, this.email});
+
+  void printUserDetails() {
+    String ageStr = age?.toString() ?? 'N/A';
+    String emailStr = email ?? 'N/A';
+    print('Name: $name, Age: $ageStr, Email: $emailStr');
+  }
+
+
+  
 }
