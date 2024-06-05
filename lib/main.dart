d8b46e2aa1366a230c7449de8c66b4ea4eeb1002
tirex300 <thetirex29@gmail.com>
2024-06-05 19:44:00 +0300

    lab2

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:group_button/group_button.dart';
 
 void main() {
+  // First task
+  var book = Book("ABC", "bob", 100);
+  book.bookDetails();
+
+  // Second task
+  var fictionBook = FictionBook("Fiction", "Abc", "bob", 15);
+  fictionBook.bookDetails();
+
+  // Third task
+  print(book.toJson());
+
+  // Fourth task
+  print(calculateArea(length: 12, width: 5));
+
+  // Fifth task
+  var user = User("Vasya", email: "ASDkasldkasod");
+  user.printUserDetails();
+
+  // App runner
   runApp(const MainApp());
 }
+
+class User {
+  String name;
+  int? age;
+  String? email;
+
+  User(this.name, {this.age, this.email});
+  void printUserDetails() {
+    print("Name: [$name]\nAge: [${age ?? "N/A"}]\nEmail: [${email ?? "N/A"}]");
+  }
+
+}
+
+double calculateArea({required double length, required double width, String shape = "rectangle"}) {
+  if (shape == "rectangle") {
+    return length * width;
+    } else if (shape == "circle") {
+      return 3.14 * length * length;
+    } else {
+      return 0.0;
+    }
+  }
+
+mixin JsonSerializable {
+  String? title;
+  String? author;
+  int? numberOfPages;
+
+  Map<String, dynamic> toJson() {
+    return {"title": title, 
+            "author" : author, 
+            "numberOfPages" : numberOfPages};
+  }
+}
+
+class FictionBook extends Book {
+  String genre;
+
+  FictionBook(this.genre, String title, String author, int numberOfPages)
+   : super(title, author, numberOfPages);
+
+  @override
+  void bookDetails() {
+    // TODO: implement bookDetails
+    print("genre - $genre, author - $author, title - $title, pages number - $numberOfPages");
+  }
+}
+
+class Book with JsonSerializable {
+  final String title;
+  final String author;
+  final int numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print('author - $author, title - $title, pages number - $numberOfPages');
+  }
+}
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,108 @@
-class Book {
-  // your code here
+import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
+
+void main() {
+  // First task
+  var book = Book("ABC", "bob", 100);
+  book.bookDetails();
+
+  // Second task
+  var fictionBook = FictionBook("Fiction", "Abc", "bob", 15);
+  fictionBook.bookDetails();
+
+  // Third task
+  print(book.toJson());
+
+  // Fourth task
+  print(calculateArea(length: 12, width: 5));
+
+  // Fifth task
+  var user = User("Vasya", email: "ASDkasldkasod");
+  user.printUserDetails();
+
+  // App runner
+  runApp(const MainApp());
 }
 
-class FictionBook {
-  // your code here
+class User {
+  String name;
+  int? age;
+  String? email;
+
+  User(this.name, {this.age, this.email});
+  void printUserDetails() {
+    print("Name: [$name]\nAge: [${age ?? "N/A"}]\nEmail: [${email ?? "N/A"}]");
+  }
+
 }
+
+double calculateArea({required double length, required double width, String shape = "rectangle"}) {
+  if (shape == "rectangle") {
+    return length * width;
+    } else if (shape == "circle") {
+      return 3.14 * length * length;
+    } else {
+      return 0.0;
+    }
+  }
 
 mixin JsonSerializable {
-  // your code here
+  String? title;
+  String? author;
+  int? numberOfPages;
+
+  Map<String, dynamic> toJson() {
+    return {"title": title, 
+            "author" : author, 
+            "numberOfPages" : numberOfPages};
+  }
 }
 
-double calculateArea(){
-  // your code here
+class FictionBook extends Book {
+  String genre;
+
+  FictionBook(this.genre, String title, String author, int numberOfPages)
+   : super(title, author, numberOfPages);
+
+  @override
+  void bookDetails() {
+    // TODO: implement bookDetails
+    print("genre - $genre, author - $author, title - $title, pages number - $numberOfPages");
+  }
 }
 
-class User{
-  // your code here
+class Book with JsonSerializable {
+  final String title;
+  final String author;
+  final int numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print('author - $author, title - $title, pages number - $numberOfPages');
+  }
+}
+
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text('Hello World!'),
+              GroupButton(
+                isRadio: false,
+                buttons: ["12:00", "13:00", "14:30", "18:00", "19:00", "21:40"],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
