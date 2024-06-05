d25ec78458143af04bb1e973467ffc6be354587b
Sviatoslav Fediaev <s.fediaev@innopolis.university>
2024-06-05 19:55:04 +0300

    update

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -8,8 +8,14 @@ class Book with JsonSerializable {
   }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+  String genre;
+  FictionBook(String title, String author, int numberOfPages, this.genre)
+      : super(title, author, numberOfPages);
+  @override
+  void bookDetails() {
+    print("$title $author $numberOfPages $genre");
+  }
 }
 
 mixin JsonSerializable {
@@ -26,10 +32,40 @@ mixin JsonSerializable {
   }
 }
 
-double calculateArea() {
-  // your code here
+double calculateArea(
+    {required double length,
+    required double? width,
+    String? shape = 'rectangle'}) {
+  if (shape == 'circle') {
+    double pi = 3.14;
+    return pi * length * length;
+  } else {
+    return length * width!;
+  }
 }
 
 class User {
-  // your code here
+  String name;
+  int? age;
+  String? email;
+  User(this.name, [this.age, this.email]);
+  void printUserDetails() {
+    String ageString = age == null ? 'N/A' : age.toString();
+    email = email ?? "N/A";
+    print("Name: $name Age: $ageString Email: $email");
+  }
+}
+
+void main() {
+  Book book = Book("Javlon", "qdqw", 19);
+  book.bookDetails();
+  print(book.toJson()); // Verify toJson for Book
+
+  FictionBook fiction = FictionBook("asd", "12", 19, "afsqw");
+  fiction.bookDetails();
+  print(fiction.toJson());
+  double area;
+  print(area = calculateArea(length: 21, width: 12));
+  User user = User("Jav");
+  user.printUserDetails();
 }
