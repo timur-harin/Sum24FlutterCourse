51b5134b08fd8e4dbae9d317e0f39108cbabe6e0
Mr_DeBuFF <a.bairamov@innopolis.university>
2024-06-05 19:04:05 +0300

    complete lab2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,99 @@
-class Book {
-  // your code here
+class Book with JsonSerializable {
+  String? title;
+  String? author;
+  int? numOfPages;
+
+  Book(this.title, this.author, this.numOfPages);
+
+  void bookDetails() {
+    print('title: $title, author: $author, numOfPages: $numOfPages');
+  }
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+  String? genre;
+  
+
+  FictionBook(String title, String author, int numOfPages, String genre) : super(title, author, numOfPages) {
+    this.genre = genre;
+  }
+
+  @override
+  void bookDetails() {
+    print('title: $title, author: $author, numOfPages: $numOfPages, genre: $genre');
+  }
+
+
 }
 
 mixin JsonSerializable {
-  // your code here
+  String? title;
+  String? author;
+  int? numOfPages;
+
+  Map<String, dynamic> toJson() {
+      return {
+        'title': this.title,
+        'author': this.author,
+        'numOfPages': this.numOfPages
+      };
+  }
 }
 
-double calculateArea(){
-  // your code here
+double calculateArea(double length, double width, {String shape = "rectangle"}) {
+  if (shape == "rectangle") {
+    return length * width;
+  } else if (shape == "circle") {
+    return 3.14 * (length * length);
+  } else {
+    return 0;
+  }
 }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+
+  User(this.name, this.age, this.email);
+
+  void printUserDetails() {
+    print('Name: $name');
+    if (age != null) {
+      print('Age: $age');
+    } else {
+      print('N/A');
+    }
+    if (email != null) {
+      print('Email: $email');
+    } else {
+      print('N/A');
+    }
+  }
+}
+
+void main(){
+  Book book = Book('BestBook', 'Amir', 111);
+  book.bookDetails();
+
+  FictionBook fictionBook = FictionBook('BestFiction', 'Star', 222, 'roman');
+  fictionBook.bookDetails();
+
+  Map<String, dynamic> json = fictionBook.toJson();
+  print(json);
+
+  double area = calculateArea(10, 20);
+  print('Area: $area');
+
+  double area2 = calculateArea(10, 20, shape: "circle");
+  print('Area: $area2');
+
+  User user = User('Amir', 18, 'a@a.com');
+  user.printUserDetails();
+
+  User user2 = User('Adel', null, 'a@b.com');
+  user2.printUserDetails();
+
+  User user3 = User('Rustam', 11, null);
+  user3.printUserDetails();
 }
