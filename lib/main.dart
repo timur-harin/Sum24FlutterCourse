34ec7f7b8ff636301478e2fbd6e122702b780bc8
Nadia <nadya.tarubarova@bk.ru>
2024-06-05 19:25:04 +0300

    lab2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:education/templates/lab2/lab2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:group_button/group_button.dart';
 
 void main() {
@@ -10,10 +12,46 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: 
+          Column(
+            children: [ElevatedButton(onPressed: (){
+            void onPressed() {
+              Book myBook = Book("Pride and Prejudice", "Jane Austen", 279);
+              myBook.bookDetails();
+            }
+            onPressed();
+          }, child: const Icon(Icons.favorite)),
+
+          ElevatedButton(onPressed: (){
+            void onPressed() {
+              Book myBook = FictionBook("Hobbit", "J.R.R. Tolkien", 295, "Fiction");
+              myBook.bookDetails();
+            }
+            onPressed();
+          }, child: const Icon(Icons.star)),
+          ElevatedButton(onPressed: (){
+            void onPressed() {
+              Book myBook = Book("The Dandelion Wine", "Ray Bradbury", 304);
+              print(myBook.toJson()) ;            }
+            onPressed();
+          }, child: const Icon(Icons.sunny)),
+
+          ElevatedButton(onPressed: (){
+            void onPressed() {
+              print(calculateArea(length: 35, width: 35));          }
+            onPressed();
+          }, child: const Text("Calculate Area")),
+          ElevatedButton(onPressed: (){
+            void onPressed() {
+                User user = User(name: 'Mary', age: 30, email: 'mary@sad.com');
+                user.printUserDetails();           }
+            onPressed();
+          }, child: const Text("User Details")),
+          ],
+          )
         ),
       ),
     );
--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,56 @@
-class Book {
-  // your code here
-}
+class Book with JsonSerializable {
+  String title;
+  String author;
+  int numberOfPages;
+  
+  Book(this.title, this.author, this.numberOfPages);
+ void bookDetails(){
+  print("$title, $author, $numberOfPages");
+ } 
+  }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book with JsonSerializable {
+  String genre;
+
+  FictionBook(String title,  String author, int numberOfPages, this.genre) : super(title, author, numberOfPages);
+  @override
+  void bookDetails(){
+    print("$title, $author, $numberOfPages, $genre");
+  }
 }
 
 mixin JsonSerializable {
-  // your code here
+String get title;
+String get author;
+int get numberOfPages;
+
+Map<String, dynamic> toJson() => {
+  'title': title,
+  'author': author,
+  'numberOfPages': numberOfPages,
+};
 }
 
-double calculateArea(){
-  // your code here
+
+double calculateArea({required length, required width, String shape = "rectangle"}) {
+if (shape == 'circle') {
+    return 3.14 * length * length; 
+  } else {
+    return length * width; 
+  }
 }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+  User({required this.name, this.age, this.email});
+
+  void printUserDetails() {
+    String ageString = age?.toString() ?? 'N/A';
+    String emailString = email ?? 'N/A';
+    print('Name: $name');
+    print('Age: $ageString');
+    print('Email: $emailString');
+  }
 }
