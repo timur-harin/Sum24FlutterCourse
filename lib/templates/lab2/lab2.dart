e921c9280de788ba28628852558b169e38b1a757
karishka1222 <123330010+karishka1222@users.noreply.github.com>
2024-06-05 20:42:11 +0300

    lab2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,69 @@
-class Book {
-  // your code here
+class Book with JsonSerializable {
+  String? title;
+  String? author;
+  int? numberOfPages;
+  Book(this.title, this.author, this.numberOfPages);
+  void bookDetails() {
+    print("title: $title\nauthor: $author\nnumber of pages: $numberOfPages");
+  }
+
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+  String genre;
+  FictionBook(String title, String author, int numberOfPages, this.genre) : super(title, author, numberOfPages);
+  @override
+  void bookDetails() {
+    print("genre: $genre\ntitle: $title\nauthor: $author\nnumber of pages: $numberOfPages");
+  }
 }
 
 mixin JsonSerializable {
-  // your code here
+  String? title;
+  String? author;
+  int? numberOfPages;
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'author': author,
+      'numberOfPages': numberOfPages
+    };
+  }
 }
 
-double calculateArea(){
-  // your code here
+double calculateArea({double? length, double? width, String shape = "rectangle"}) {
+  if (shape == "circle") {
+    return 3.14 * length! * length!;
+  } else {
+    return length! * width!;
+  }
+  
 }
 
-class User{
-  // your code here
+class User {
+  String name = "";
+  int? age;
+  String? email;
+  User({required String name, int? age, String? email}) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+  void printUserDetails() {
+    dynamic a = age ?? "N/A";
+    dynamic b = email ?? "N/A";
+    print("Name: $name Age: $a email Email: $b");
+  }
+}
+
+void main() {
+  Book book = Book("Harry Potter", "JK Rowling", 400);
+  book.bookDetails();
+
+
+  FictionBook fictionBook = FictionBook("It", "Stiven King", 100, "Horror");
+  fictionBook.bookDetails();
+  print(calculateArea(length: 5, width: 10));
+  User user = User(name: "John", age: 25, email: "0WJpj@example.com");
+  user.printUserDetails();
 }
