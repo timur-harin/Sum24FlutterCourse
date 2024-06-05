fe8c70214acab375acc70cdc92cacf238b060a83
CAN4red <CAN4red@yandex.ru>
2024-06-05 20:07:12 +0500

    First task

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,5 +1,22 @@
+import 'package:flutter/foundation.dart';
+
+void main() {
+  Book book = Book("Pu-pu-puu", "Aleksandr Isupov", 21);
+  book.bookDetails();
+}
+
 class Book {
-  // your code here
+  String? title;
+  String? author;
+  int? numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  bookDetails() {
+    print("Title: $title\n"
+        "Author: $author\n"
+        "Number of pages: $numberOfPages");
+  }
 }
 
 class FictionBook {
@@ -10,10 +27,11 @@ mixin JsonSerializable {
   // your code here
 }
 
-double calculateArea(){
+double calculateArea() {
+  return 1.0;
   // your code here
 }
 
-class User{
+class User {
   // your code here
 }
