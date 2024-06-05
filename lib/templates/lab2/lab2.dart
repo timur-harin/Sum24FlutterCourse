6c6354017a45dbfe9ac8211cf0ff3468faffb5be
Стася Малахова <stasiamalakhova@MacBook-Air-Stasa.local>
2024-06-05 19:07:32 +0300

    lab2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,87 @@
-class Book {
-  // your code here
+class Book with JsonSerializable {
+  final String? title; 
+  final String? author; 
+  final int? numberOfPages;
+
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails(){
+    print ("Title: $title, Author: $author, Number of pages: $numberOfPages");
+  }
+
 }
 
-class FictionBook {
-  // your code here
+class FictionBook extends Book {
+
+   String? genre;
+
+  FictionBook(String title, String author, int numberOfPages, String genre) : super(title, author, numberOfPages){
+    this.genre = genre;
+  }
+
+  @override
+  void bookDetails() {
+    print ("Title: $title, Author: $author, Number of pages: $numberOfPages, genre: $genre");
+  }
 }
 
 mixin JsonSerializable {
-  // your code here
+  String? title; 
+  String? author; 
+  int? numberOfPages;
+
+
+  Map<String, dynamic> toJson() => {
+    'title': title,
+    'author': author,
+    'numberOfPages': numberOfPages
+  };
 }
 
-double calculateArea(){
-  // your code here
-}
+
+
+  calculateArea(double lenght, double width,{String shape = "rectangle"}){  
+    if(shape == "circle"){
+      return 3.14 * lenght * lenght;
+  
+    }
+    return lenght * width;
+  
+  }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+
+  User(this.name, this.age, this.email);
+
+  printUserDetails(){
+   String userName = name ?? "N/A";
+   String userAge = age?.toString() ?? "N/A";
+   String userEmail = email ?? "N/A";
+   print("Name: $userName");
+   print("Age: $userAge");
+   print("Email: $userEmail");
+  }
+}
+
+void main() {
+  var story = Book("The Hobbit", "J.R.R. Tolkien", 295);
+  story.bookDetails();
+  print(story.toJson());
+
+  var fiction = FictionBook("The Hobbit", "J.R.R. Tolkien", 295, "Fantasy");
+  fiction.bookDetails();  
+
+
+  print(calculateArea(10, 20));
+  print(calculateArea(10, 20, shape: "circle"));
+
+
+  var user = User("John", 30, "qKwXU@example.com");
+  user.printUserDetails();
+
+
+
 }
