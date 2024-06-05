87baaa86b6b685be61b71b717411778fcd920320
Alisher Kamolov <alisherkamolov9999@gmail.com>
2024-06-05 20:10:41 +0300

    complete lab2

--- lib/templates/lab2/lab2.dart
+++ lib/templates/lab2/lab2.dart
@@ -1,19 +1,81 @@
-class Book {
-  // your code here
+
+
+void main() {
+  FictionBook fictionBook = FictionBook('Lean Startup', 'Eric Ries', 366, 'Non-fiction');
+  //var book1 = Book('Lean Startup', 'Eric Ries', 366);
+  fictionBook.bookDetails();
+  print('\n');
+  print(fictionBook.toJson());
+
 }
 
-class FictionBook {
-  // your code here
+class Book with JsonSerializable  {
+  String title;
+  String author;
+  int numberOfPages;
+  
+  Book(this.title, this.author, this.numberOfPages);
+
+  void bookDetails() {
+    print('Title: $title');
+    print('Author: $author');
+    print('Number of pages: $numberOfPages');
+  }
+}
+
+class FictionBook extends Book{
+  String genre;
+
+  FictionBook(super.title, super.author, super.numberOfPages, this.genre);
+  
+  @override 
+  void bookDetails() {
+    super.bookDetails();
+    print('Genre: $genre');
+  }
 }
 
 mixin JsonSerializable {
-  // your code here
+
+  String get title;
+  String get author;
+  int get numberOfPages;
+  
+  Map<String, dynamic> toJson() => {
+    'title' : title,
+    'author': author,
+    'numberofPages': numberOfPages,
+  };
 }
 
-double calculateArea(){
-  // your code here
+double calculateArea(double length, double width, String shape){
+  //Rectangle
+  double area;
+  if(shape == 'circle') {
+    area = 3.14 * length * length;
+  } else {
+    area = length * width;
+  }
+  return area;
 }
 
 class User{
-  // your code here
+  String name;
+  int? age;
+  String? email;
+  
+  User(this.name, this.age, this.email);
+
+  void printUserDetails() {
+    print('Name: $name');
+    print('Age: ${age ?? 'N/A'}');
+    print('Email: ${email ?? 'N/A'}');
+
+  }
 }
+
+
+
+// git add.
+// git commit -m "comlete lab2"
+// git push
