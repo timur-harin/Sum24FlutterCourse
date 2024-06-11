34e791f53209423983e4ac3a0dfe2a53e6c6ceed
Sawolf <Savipon@yandex.ru>
2024-06-11 19:20:06 +0300

    lab3

--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -9,20 +9,42 @@ class HydrationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    final waterIntake = ref.watch(waterIntakeProvider);
     return Scaffold(
       // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        title: const Text('Water Balance'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(waterIntakeProvider.notifier).reset();
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // TODO - Add text to display the water intake
+            Text(waterIntake.toString()),
             // TODO add HydrationWidget to display the water intake and put waterIntake into it
+            HydrationWidget(waterIntakeLevel: waterIntake),
+
             // Add more UI components if necessary
           ],
         ),
       ),
       // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(waterIntakeProvider.notifier).increment(250);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
