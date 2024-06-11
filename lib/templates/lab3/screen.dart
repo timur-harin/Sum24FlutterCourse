83ecf08fdaf1d30cfcd3810dba67137fefe132cb
meldilen <123398352+meldilen@users.noreply.github.com>
2024-06-11 20:53:23 +0300

    lab3

--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -9,20 +9,41 @@ class HydrationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    final waterIntake = ref.watch(waterIntakeProvider);
     return Scaffold(
       // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => ref.read(waterIntakeProvider.notifier).reset(),
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // TODO - Add text to display the water intake
+            Text(
+              "Water Intake: ${waterIntake} ml",
+              style: TextStyle(
+                  fontSize: 100,
+                  color: const Color.fromARGB(255, 211, 151, 147)),
+            ),
             // TODO add HydrationWidget to display the water intake and put waterIntake into it
             // Add more UI components if necessary
+            const SizedBox(height: 24),
+            HydrationWidget(waterIntakeLevel: waterIntake),
           ],
         ),
       ),
       // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(waterIntakeProvider.notifier).increment(15),
+        tooltip: "Water Intake incremention",
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
--- lib/templates/lab3/storage.dart
+++ lib/templates/lab3/storage.dart
@@ -6,11 +6,12 @@ class LocalStorageService {
   Future<void> saveWaterIntake(double intake) async {
     final prefs = await SharedPreferences.getInstance();
     // TODO - Save the water intake into prefs  
+    await prefs.setDouble(_waterIntakeKey, intake);
   }
 
   Future<double> getWaterIntake() async {
     final prefs = await SharedPreferences.getInstance();
     // TODO - Get the water intake from prefs, remove 'return 0.0'
-    return 0.0;
+    return prefs.getDouble(_waterIntakeKey) ?? 0.0;
   }
 }
