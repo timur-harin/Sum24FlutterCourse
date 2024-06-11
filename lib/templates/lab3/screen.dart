8086282331610d5b1c3cb17d78e6fba181783058
Borodum <batraz@dzesov.ru>
2024-06-11 20:21:57 +0300

    lab3

--- lib/templates/lab3/screen.dart
+++ lib/templates/lab3/screen.dart
@@ -9,20 +9,42 @@ class HydrationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO - Add ref.watch and use provider to get the water intake
-    // final waterIntake =
+    final waterIntake = ref.watch(waterIntakeProvider);
+
     return Scaffold(
       // TODO add AppBar with Icon to reset the water intake as actions parameter of AppBar
+      appBar: AppBar(
+        title: Text("Aqua Max"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        actions: <Widget>[],
+        leading: IconButton(
+          onPressed: (){
+            ref.read(waterIntakeProvider.notifier).reset();
+          },
+          icon: Icon(Icons.refresh),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // TODO - Add text to display the water intake
+            Text("Level of water: " + waterIntake.toString() + " ml"),
             // TODO add HydrationWidget to display the water intake and put waterIntake into it
+            HydrationWidget(waterIntakeLevel: waterIntake,)
             // Add more UI components if necessary
           ],
         ),
       ),
       // TODO - Add floating action button to increment the water intake using ref.read(waterIntakeProvider.notifier).increment(x)
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
+        onPressed: (){ref.read(waterIntakeProvider.notifier).increment(100);},
+      ),
+
     );
   }
 }
