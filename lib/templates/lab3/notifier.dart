64935f9331ce4530b5b926a20965953d19700b11
Javlon <u.bakhodirov@innopolis.univeristy>
2024-06-11 19:58:35 +0300

    lab 3

--- lib/templates/lab3/notifier.dart
+++ lib/templates/lab3/notifier.dart
@@ -19,16 +19,21 @@ class WaterIntakeNotifier extends StateNotifier<double> {
   }
 
   void _loadWaterIntake() async {
-    // TODO - Load the water intake from _localStorageService using await
-    // state =
+    state = await _localStorageService.getWaterIntake();
   }
 
   void increment(double amount) async {
-    state += amount;
-    // TODO - Save the water intake into _localStorageService using saveWaterIntake
+    if (state >= 1.5) {
+      state = 1.5;
+    } else {
+      state += amount;
+    }
+
+    await _localStorageService.saveWaterIntake(state);
   }
 
   void reset() async {
-    // TODO - reset state and save it into _localStorageService using saveWaterIntake
+    state = 0;
+    await _localStorageService.saveWaterIntake(state);
   }
 }
