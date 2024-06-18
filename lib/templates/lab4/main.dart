97149508cfcb6f68d710e83ba68ab1aaea3da673
Rail <rai.sabirov@innopolis.university>
2024-06-18 22:03:39 +0300

    lab4

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -1,26 +1,39 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' hide Provider;
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
+import 'package:provider/provider.dart' hide ChangeNotifierProvider;
+
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
+    return const ProviderScope(
       child: MaterialApp(
         home: MyHomePage(),
       ),
     );
   }
 }
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
 
-class MyHomePage extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    //Does not work provider, idk why
+    // final counterProvider = Provider.of<Counter>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -33,15 +46,18 @@ class MyHomePage extends StatelessWidget {
               onPressed: () async {
                 // TODO
                 // Exercise 1 - Perform an async operation using async/await
-                String result = await fetchData();
-                print(result);
+                String response = await fetchData();
+                ref.read(resultFROMJSON.notifier).state = response;
               },
               child: Text('Async/Await Task'),
             ),
             ElevatedButton(
               onPressed: () {
                 // Exercise 2 - Use Provider for state management
+
                 // Increment the counter
+                // Provider does not work, idk why
+                // counterProvider.increment();
               },
               child: Text('Provider Task'),
             ),
@@ -50,6 +66,7 @@ class MyHomePage extends StatelessWidget {
                 // TODO
                 // Exercise 3 - Use Riverpod for state management
                 // Increment the counter
+                ref.read(counterRiverpod.notifier).increment();
               },
               child: Text('Riverpod Task'),
             ),
@@ -57,6 +74,8 @@ class MyHomePage extends StatelessWidget {
               onPressed: () async {
                 // TODO 
                 // Exercise 4 - Make an HTTP request using the HTTP package
+                final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+                ref.read(resultFROMJSON.notifier).state = response.body;
               },
               child: Text('HTTP Task'),
             ),
@@ -64,9 +83,15 @@ class MyHomePage extends StatelessWidget {
               onPressed: () async {
                 // TODO
                 // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                final dio = Dio();
+                final response = await dio.get('https://jsonplaceholder.typicode.com/posts/1');
+                ref.read(resultFROMJSON.notifier).state = response.data.toString();
               },
               child: Text('Dio Task'),
             ),
+            Text("Riverpod Counter: ${ref.watch(counterRiverpod)}"),
+            Text("Result from JSON: ${ref.watch(resultFROMJSON)}"),
+            // Text("Provider counter: ${counterProvider.counter}"),
           ],
         ),
       ),
@@ -77,13 +102,57 @@ class MyHomePage extends StatelessWidget {
 Future<String> fetchData() async {
   // TODO get json from url and show as text
   // 'https://jsonplaceholder.typicode.com/posts/1'
+  final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
 
-  return 'data';
+  final response = await http.get(url);
+  if (response.statusCode == 200) {  
+    return response.body;
+  } else {
+    throw Exception('Failed to load data');
+  }
 }
 
-final counterProvider = StateProvider<int>((ref) => 0);
+final counterRiverpod = StateNotifierProvider<CounterNotifier,int>((ref) => CounterNotifier());
 
 // TODO create a state notifier
 // final 
 
-// TODO create class for state notifier
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier() : super(0);
+
+  int _counter = 0;
+
+  void increment() {
+    _counter++;
+    state = _counter;
+  }
+
+  int get counter => _counter;  
+  set counter(int value) => _counter = value;
+}
+
+final resultFROMJSON = StateNotifierProvider<resultFROMJSONNotifier, String>((ref) => resultFROMJSONNotifier());
+
+
+class resultFROMJSONNotifier extends StateNotifier<String> {
+  resultFROMJSONNotifier() : super('');
+
+  String _result = '';
+
+  String get result => _result;
+  set result(String value) => _result = value;
+}
+
+// Does not work, idk why
+// class Counter extends ChangeNotifier{
+//   int _counter = 0;
+
+//   void increment() {
+//     _counter++;
+//     notifyListeners();
+//   }
+
+//   int get counter => _counter;  
+//   set counter(int value) => _counter = value;
+// }
+
