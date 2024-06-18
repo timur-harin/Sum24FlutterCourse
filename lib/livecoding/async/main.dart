77fe6c49f8f41be0c6ea7d3ab9b2492a6c04a8b7
Gurbanberdi Gulladyyev <gurbangulladyyev24@gmail.com>
2024-06-18 22:46:17 +0300

    lab4

--- lib/livecoding/async/main.dart
+++ lib/livecoding/async/main.dart
@@ -1,5 +1,4 @@
-
-// import 'package:http/http.dart';
+import 'package:http/http.dart';
 import 'package:dio/dio.dart';
 
 import 'package:flutter/material.dart';
@@ -8,110 +7,112 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   getJson();
-  runApp(ProviderScope(child: MyApp()));
+  // runApp(ProviderScope(child: MyApp()));
 }
 
 void getJson() async {
-    Dio dio = Dio();
+  // Dio dio = Dio();
 
   final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
 
-  final response = await dio.get(url.toString());
-
+  final response = await get(url);
   if (response.statusCode == 200) {
-    print(response.data);
-  }
-  else {
-    print('Request failed with status: ${response.statusCode}.');
+    print('Success');
   }
 
+  print(123);
+
+  // final response = await dio.get(url.toString());
+
+  // if (response.statusCode == 200) {
+  //   print(response.data);
+  // } else {
+  //   print('Request failed with status: ${response.statusCode}.');
+  // }
 }
 
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       initialRoute: '/one',
+//       routes: {
+//         '/one': (context) => const FirstPage(),
+//         '/two': (context) => const SecondPage(),
+//       },
+//     );
+//   }
+// }
 
+// final moveProvider = StateNotifierProvider<MoveNotifier, int>((ref) {
+//   return MoveNotifier();
+// });
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/one',
-      routes: {
-        '/one': (context) => const FirstPage(),
-        '/two': (context) => const SecondPage(),
-      },
-    );
-  }
-}
+// class MoveNotifier extends StateNotifier<int> {
+//   MoveNotifier() : super(0);
 
-final moveProvider = StateNotifierProvider<MoveNotifier, int>((ref) {
-  return MoveNotifier();
-});
+//   int _moveCount = 0;
 
-class MoveNotifier extends StateNotifier<int> {
-  MoveNotifier() : super(0);
+//   void move() {
+//     _moveCount++;
+//     state = _moveCount;
+//   }
 
-  int _moveCount = 0;
+//   int get moveCount => _moveCount;
 
-  void move() {
-    _moveCount++;
-    state = _moveCount;
-  }
+//   set moveCount(int value) => _moveCount = value;
+// }
 
-  int get moveCount => _moveCount;
+// class FirstPage extends ConsumerStatefulWidget {
+//   const FirstPage({super.key});
 
-  set moveCount(int value) => _moveCount = value;
-}
+//   @override
+//   ConsumerState<ConsumerStatefulWidget> createState() => _FirstPageState();
+// }
 
-class FirstPage extends ConsumerStatefulWidget {
-  const FirstPage({super.key});
+// class _FirstPageState extends ConsumerState<FirstPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text(
+//           'Move Count: ${ref.watch(moveProvider)}',
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           ref.read(moveProvider.notifier).move();
+//           Navigator.pushNamed(context, '/two');
+//         },
+//         child: const Icon(Icons.arrow_forward),
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _FirstPageState();
-}
+// class SecondPage extends ConsumerStatefulWidget {
+//   const SecondPage({super.key});
 
-class _FirstPageState extends ConsumerState<FirstPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Move Count: ${ref.watch(moveProvider)}',
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(moveProvider.notifier).move();
-          Navigator.pushNamed(context, '/two');
-        },
-        child: const Icon(Icons.arrow_forward),
-      ),
-    );
-  }
-}
+//   @override
+//   ConsumerState<ConsumerStatefulWidget> createState() => _SecondPageState();
+// }
 
-class SecondPage extends ConsumerStatefulWidget {
-  const SecondPage({super.key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _SecondPageState();
-}
-
-class _SecondPageState extends ConsumerState<SecondPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Move Count: ${ref.watch(moveProvider)}',
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(moveProvider.notifier).move();
-          Navigator.pop(context);
-        },
-        child: const Icon(Icons.arrow_back),
-      ),
-    );
-  }
-}
-
+// class _SecondPageState extends ConsumerState<SecondPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text(
+//           'Move Count: ${ref.watch(moveProvider)}',
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           ref.read(moveProvider.notifier).move();
+//           Navigator.pop(context);
+//         },
+//         child: const Icon(Icons.arrow_back),
+//       ),
+//     );
+//   }
+// }
--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -1,7 +1,12 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'
+    hide ChangeNotifierProvider;
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,9 +23,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -50,13 +55,22 @@ class MyHomePage extends StatelessWidget {
                 // TODO
                 // Exercise 3 - Use Riverpod for state management
                 // Increment the counter
+                ref.read(counterProvider.notifier).increment();
+                print(ref.watch(counterProvider));
               },
               child: Text('Riverpod Task'),
             ),
             ElevatedButton(
               onPressed: () async {
-                // TODO 
+                // TODO
                 // Exercise 4 - Make an HTTP request using the HTTP package
+                final response = await get(
+                    Uri.parse('http://jsonplaceholder.typicode.com/albums/1'));
+                if (response.statusCode == 200) {
+                  print(response.body);
+                } else {
+                  print('Failed to fetch');
+                }
               },
               child: Text('HTTP Task'),
             ),
@@ -64,6 +78,14 @@ class MyHomePage extends StatelessWidget {
               onPressed: () async {
                 // TODO
                 // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                Dio dio = Dio();
+                final response =
+                    await dio.get('https://jsonplaceholder.org/users');
+                if (response.statusCode == 200) {
+                  print(response.data);
+                } else {
+                  print('Falied to fetch');
+                }
               },
               child: Text('Dio Task'),
             ),
@@ -77,13 +99,23 @@ class MyHomePage extends StatelessWidget {
 Future<String> fetchData() async {
   // TODO get json from url and show as text
   // 'https://jsonplaceholder.typicode.com/posts/1'
+  final url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+  final response = await get(url);
 
-  return 'data';
+  return response.body;
 }
 
-final counterProvider = StateProvider<int>((ref) => 0);
+final counterProvider =
+    StateNotifierProvider<Counter, int>((ref) => (Counter()));
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+  void increment() {
+    state++;
+  }
+}
 
 // TODO create a state notifier
-// final 
+// final
 
 // TODO create class for state notifier
