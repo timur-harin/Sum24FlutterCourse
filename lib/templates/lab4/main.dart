3fd4fe8baed4027a350952b391f0c6ecf1b3bf88
Cdeth567 <11kvvkvv11@mail.ru>
2024-06-18 21:30:11 +0300

    lab4

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -34,39 +34,63 @@ class MyHomePage extends StatelessWidget {
                 // TODO
                 // Exercise 1 - Perform an async operation using async/await
                 String result = await fetchData();
-                print(result);
+                ref.read(jsonProvider.notifier).state = result;
               },
               child: Text('Async/Await Task'),
             ),
+            SizedBox(height: 10),
+            Text(ref.watch(jsonProvider)),
+            SizedBox(height: 10,),
             ElevatedButton(
               onPressed: () {
                 // Exercise 2 - Use Provider for state management
                 // Increment the counter
+                ref.read(counterProvider.notifier).state++;
               },
-              child: Text('Provider Task'),
+              child: Text(ref.watch(counterProvider).toString()),
+              // Text('Provider Task'),
             ),
+            SizedBox(height: 10),
+            /////////////////////////////
+            
+            SizedBox(height: 10,),
             ElevatedButton(
               onPressed: () {
                 // TODO
                 // Exercise 3 - Use Riverpod for state management
                 // Increment the counter
+                ref.read(moveProvider.notifier).increment();
               },
-              child: Text('Riverpod Task'),
+              child: Text(ref.watch(moveProvider).toString()),
+              // Text('Riverpod Task'),
             ),
+            SizedBox(height: 10),
+            ///////////////////////////
+            
+            SizedBox(height: 10,),
             ElevatedButton(
               onPressed: () async {
                 // TODO 
                 // Exercise 4 - Make an HTTP request using the HTTP package
+                String result = await fetchData();
+                ref.read(httpProvider.notifier).state = result;
               },
               child: Text('HTTP Task'),
             ),
+            SizedBox(height: 10),
+            Text(ref.watch(httpProvider)),
+            SizedBox(height: 10,),
             ElevatedButton(
               onPressed: () async {
                 // TODO
                 // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                Response response = await Dio().get('https://jsonplaceholder.typicode.com/posts/1');
+                ref.read(dioProvider.notifier).state = response.data.toString();
               },
               child: Text('Dio Task'),
             ),
+            SizedBox(height: 10),
+            Text(ref.watch(dioProvider)),
           ],
         ),
       ),
@@ -78,12 +102,32 @@ Future<String> fetchData() async {
   // TODO get json from url and show as text
   // 'https://jsonplaceholder.typicode.com/posts/1'
 
-  return 'data';
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  else {
+    throw Exception('Request failed with status: ${response.statusCode}.');
+  }
+  // return 'data';
 }
 
 final counterProvider = StateProvider<int>((ref) => 0);
+final jsonProvider = StateProvider<String>((ref) => '');
+final httpProvider = StateProvider<String>((ref) => '');
+final dioProvider = StateProvider<String>((ref) => '');
 
 // TODO create a state notifier
-// final 
+final moveProvider = StateNotifierProvider<MoveNotifier, int>((ref) {
+  return MoveNotifier();
+});
 
 // TODO create class for state notifier
+class MoveNotifier extends StateNotifier<int> {
+  MoveNotifier() : super(0);
+
+  void increment() {
+    state++;
+  }
+}
