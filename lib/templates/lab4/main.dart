c8af85ef0ae16bada5e7cf7ba09527eea3322ff1
Resent <remal11151@gmail.com>
2024-06-18 22:36:34 +0300

    lab4 done

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -42,28 +42,50 @@ class MyHomePage extends StatelessWidget {
               onPressed: () {
                 // Exercise 2 - Use Provider for state management
                 // Increment the counter
+                ref.read(counterProvider.notifier).state++;
               },
               child: Text('Provider Task'),
             ),
+            Text("Counter: ${ref.watch(counterProvider)}"),
             ElevatedButton(
               onPressed: () {
                 // TODO
                 // Exercise 3 - Use Riverpod for state management
-                // Increment the counter
+                ref.read(stateNotifier.notifier).increment();
               },
               child: Text('Riverpod Task'),
             ),
+            Text("Counter: ${ref.watch(stateNotifier)}"),
             ElevatedButton(
               onPressed: () async {
-                // TODO 
                 // Exercise 4 - Make an HTTP request using the HTTP package
+                final response = await http.get(
+                    Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Text('Using HTTP response:${response.body}'),
+                      );
+                    });
               },
               child: Text('HTTP Task'),
             ),
             ElevatedButton(
               onPressed: () async {
-                // TODO
                 // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+
+                final dio = Dio();
+                final response = await dio
+                    .get('https://jsonplaceholder.typicode.com/posts/1');
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Text('Using DIO response:${response.data}'),
+                    );
+                  },
+                );
               },
               child: Text('Dio Task'),
             ),
@@ -78,12 +100,30 @@ Future<String> fetchData() async {
   // TODO get json from url and show as text
   // 'https://jsonplaceholder.typicode.com/posts/1'
 
-  return 'data';
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+  if (response.statusCode == 200) {
+    return response.body.toString();
+  } else {
+    throw Exception('Failed to load data');
+  }
 }
 
 final counterProvider = StateProvider<int>((ref) => 0);
 
 // TODO create a state notifier
-// final 
+final stateNotifier = StateNotifierProvider<CounterNotifier, int>((ref) {
+  return CounterNotifier();
+});
 
 // TODO create class for state notifier
+class CounterNotifier extends StateNotifier<int> {
+  int _counter = 0;
+
+  CounterNotifier() : super(0);
+
+  void increment() {
+    _counter++;
+    state = _counter;
+  }
+}
