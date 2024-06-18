45483414240e431ab9fd553f750ec3b6fcedc7d1
arthur <ar.popov@innopolis.university>
2024-06-18 20:16:58 +0300

    lab4

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -18,9 +18,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+
+class MyHomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Tasks'),
@@ -31,41 +32,60 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             ElevatedButton(
               onPressed: () async {
-                // TODO
+                // DONE
                 // Exercise 1 - Perform an async operation using async/await
                 String result = await fetchData();
-                print(result);
+                ref.read(jsonProvider.notifier).state = result;
               },
               child: Text('Async/Await Task'),
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Exercise 2 - Use Provider for state management
-                // Increment the counter
-              },
-              child: Text('Provider Task'),
+            Text(
+              ref.watch(jsonProvider)
             ),
             ElevatedButton(
               onPressed: () {
-                // TODO
+                // DONE
+                // Exercise 2 - Use Provider for state management
+                // Increment the counter
+                ref.read(counterProvider.notifier).state++;
+              },
+              child: Text('Provider Task: count = ${ref.watch(counterProvider)}'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // DONE
                 // Exercise 3 - Use Riverpod for state management
                 // Increment the counter
+                ref.read(counterRiverpodProvider.notifier).increment();
               },
-              child: Text('Riverpod Task'),
+              child: Text('Riverpod Task: count = ${ref.watch(counterRiverpodProvider)}'),
             ),
             ElevatedButton(
               onPressed: () async {
-                // TODO 
+                // DONE
                 // Exercise 4 - Make an HTTP request using the HTTP package
+                ref.read(httpProvider.notifier).state = await fetchData();
               },
               child: Text('HTTP Task'),
             ),
+            Text(
+              ref.watch(httpProvider)
+            ),
             ElevatedButton(
               onPressed: () async {
-                // TODO
+                // DONE
                 // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                Response response = await Dio().get("https://jsonplaceholder.typicode.com/posts/1");
+                ref.read(imageProvider.notifier).state = NetworkImage("https://http.cat/${response.statusCode.toString()}");
+                ref.read(dioProvider.notifier).state = response.toString();
               },
               child: Text('Dio Task'),
+            ),
+            Text(
+              ref.watch(dioProvider)
+            ),
+            Image.network(
+              ref.watch(imageProvider).toString()
             ),
           ],
         ),
@@ -75,15 +95,33 @@ class MyHomePage extends StatelessWidget {
 }
 
 Future<String> fetchData() async {
-  // TODO get json from url and show as text
-  // 'https://jsonplaceholder.typicode.com/posts/1'
-
-  return 'data';
+  final url = Uri.parse("https://jsonplaceholder.typicode.com/posts/1");
+  final response = await http.get(url);
+  
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  else {
+    return 'Request failed with status: ${response.statusCode}.';
+  }
 }
 
+// default Providers
+final jsonProvider = StateProvider<String>((ref) => '');
 final counterProvider = StateProvider<int>((ref) => 0);
+final httpProvider = StateProvider<String>((ref) => '');
+final dioProvider = StateProvider<String>((ref) => '');
+final imageProvider = StateProvider<NetworkImage>((ref) => NetworkImage('https://picsum.photos/500/500?random=1'));
 
-// TODO create a state notifier
-// final 
+// riverpod Providers
+class CounterRiverpodState extends StateNotifier<int> {
+  CounterRiverpodState() : super(0);
 
-// TODO create class for state notifier
+  void increment() {
+    state++;
+  }
+}
+
+final counterRiverpodProvider = StateNotifierProvider<CounterRiverpodState, int>((ref) {
+  return CounterRiverpodState();
+});
