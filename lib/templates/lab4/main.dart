5194fdf3989366a642d0542b01464044c4bd9656
wkwtfigo <e.zagurskih@innopolis.university>
2024-06-18 21:56:26 +0300

    Lab 4

--- lib/templates/lab4/main.dart
+++ lib/templates/lab4/main.dart
@@ -34,6 +34,7 @@ class MyHomePage extends ConsumerWidget {
             ElevatedButton(
               onPressed: () async {
                 String result = await fetchData();
+                final jsonProvider = StateProvider<String>((ref) => '');
                 ref.read(jsonProvider.notifier).state = result;
                  showDialog(
                   context: context,
@@ -57,6 +58,7 @@ class MyHomePage extends ConsumerWidget {
             const SizedBox(height: 20),
             Consumer(
               builder: (context, ref, child) {
+                final counterProvider = StateProvider<int>((ref) => 0);
                 final counter = ref.watch(counterProvider);
                 return ElevatedButton(
                   onPressed: () {
@@ -82,6 +84,7 @@ class MyHomePage extends ConsumerWidget {
             ElevatedButton(
               onPressed: () async {
                 String result = await fetchData();
+                final httpProvider = StateProvider<String>((ref) => '');
                 ref.read(httpProvider.notifier).state = result;
                 showDialog(
                   context: context,
@@ -104,6 +107,7 @@ class MyHomePage extends ConsumerWidget {
             ElevatedButton(
               onPressed: () async {
                 Response response = await Dio().get('https://jsonplaceholder.typicode.com/posts/1');
+                final dioProvider = StateProvider<String>((ref) => '');
                 NetworkImage image = NetworkImage("https://http.error/${response.statusCode.toString()}");
                 ref.read(imageProvider.notifier).state = image;
                 ref.read(dioProvider.notifier).state = response.toString();
@@ -160,12 +164,8 @@ class CounterRiverpodState extends StateNotifier<int> {
   void increment() => state++;
 }
 
-final counterProvider = StateProvider<int>((ref) => 0);
-final jsonProvider = StateProvider<String>((ref) => '');
-final httpProvider = StateProvider<String>((ref) => '');
-final dioProvider = StateProvider<String>((ref) => '');
-final imageProvider = StateProvider<NetworkImage>((ref) => const NetworkImage('https://www.istockphoto.com/ru/%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8/%D0%BB%D0%B5%D1%81'));
-
 final counterRiverpodNotifierProvider = StateNotifierProvider<CounterRiverpodState, int>((ref) {
   return CounterRiverpodState();
 });
+
+final imageProvider = StateProvider<NetworkImage>((ref) => const NetworkImage('https://www.istockphoto.com/ru/%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8/%D0%BB%D0%B5%D1%81'));
