7a30074c64bcf08821a114f23d2d402e136caf50
Dmitrii-Bargan <d.bargan@innopolis.university>
2024-06-18 22:34:24 +0300

    Lab 4 task 2 and 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const ProviderScope(
+    return ProviderScope(
       child: MaterialApp(
         home: MyHomePage(),
       ),
@@ -18,101 +19,100 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
+class MyHomePage extends ConsumerWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String responseText = "No response received, yet";
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     const double widgetMargin = 8;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Tasks'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.topCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.all(widgetMargin),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      // TODO Exercise 1 - Perform an async operation using async/await
-                      setState(() {
-                        responseText = "Waiting for response...";
-                      });
-                      String result = await fetchData();
-                      setState(() {
-                        responseText = result;
-                      });
-                    },
-                    child: const Text('Async/Await Task'),
+        appBar: AppBar(
+          title: const Text('Flutter Tasks'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(widgetMargin),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        // TODO Exercise 1 - Perform an async operation using async/await
+                        ref.watch(responseProvider.notifier).state =
+                            "Loading...";
+                        String result = await fetchData();
+                        ref.watch(responseProvider.notifier).state = result;
+                      },
+                      child: const Text('Async/Await Task'),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(widgetMargin),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Exercise 2 - Use Provider for state management
-                      // Increment the counter
-                    },
-                    child: const Text('Provider Task'),
+                  Container(
+                    margin: const EdgeInsets.all(widgetMargin),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Exercise 2 - Use Provider for state management
+                        // Increment the counter
+                        ref.watch(counterProvider.notifier).state++;
+                      },
+                      child: const Text('Provider Task'),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(widgetMargin),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // TODO
-                      // Exercise 3 - Use Riverpod for state management
-                      // Increment the counter
-                    },
-                    child: const Text('Riverpod Task'),
+                  Container(
+                    margin: const EdgeInsets.all(widgetMargin),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // TODO
+                        // Exercise 3 - Use Riverpod for state management
+                        // Increment the counter
+                        ref
+                            .watch(RiverpodStateNotifierProvider.notifier)
+                            .increment();
+                      },
+                      child: const Text('Riverpod Task'),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(widgetMargin),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      // TODO
-                      // Exercise 4 - Make an HTTP request using the HTTP package
-                    },
-                    child: const Text('HTTP Task'),
+                  Container(
+                    margin: const EdgeInsets.all(widgetMargin),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        // TODO
+                        // Exercise 4 - Make an HTTP request using the HTTP package
+                      },
+                      child: const Text('HTTP Task'),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(widgetMargin),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      // TODO
-                      // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
-                    },
-                    child: const Text('Dio Task'),
+                  Container(
+                    margin: const EdgeInsets.all(widgetMargin),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        // TODO
+                        // Exercise 5 - Make an HTTP request using Dio and show it in App Screen
+                      },
+                      child: const Text('Dio Task'),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: widgetMargin, 
-                    horizontal: widgetMargin * 3
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: widgetMargin, horizontal: widgetMargin * 3),
+                    child: Text(ref.watch(responseProvider).toString()),
                   ),
-                  child: Text(responseText),
-                ),
-              ],
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: widgetMargin, horizontal: widgetMargin * 3),
+                    child: Text("Counter value: ${ref.watch(counterProvider)}"),
+                  ),
+                 Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: widgetMargin, horizontal: widgetMargin * 3),
+                    child: Text("Riverpod counter value: ${ref.watch(RiverpodStateNotifierProvider)}"),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      )
-    );
+          ],
+        ));
   }
 }
 
@@ -120,7 +120,8 @@ Future<String> fetchData() async {
   // TODO get json from url and show as text
   // 'https://jsonplaceholder.typicode.com/posts/1'
 
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
 
   if (response.statusCode == 200) {
     return response.body.toString();
@@ -130,9 +131,18 @@ Future<String> fetchData() async {
   }
 }
 
-final counterProvider = StateProvider<int>((ref) => 0);
-
 // TODO create a state notifier
-// final
+final responseProvider = StateProvider((ref) => "Nothing to show here");
+final counterProvider = StateProvider((ref) => 0);
 
 // TODO create class for state notifier
+class RiverpodStateNotifier extends StateNotifier<int> {
+  RiverpodStateNotifier([super.state = 0]);
+
+  void increment() {
+    state += 1;
+  }
+}
+
+final RiverpodStateNotifierProvider =
+    StateNotifierProvider((ref) => RiverpodStateNotifier(10));
