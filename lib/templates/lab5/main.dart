4f7914d01c2890672b80be49f59828b173cd9964
Vladimir Zhidkov <v.zhidkov@innopolis.university>
2024-06-19 22:06:34 +0300

    lab5

--- lib/templates/lab5/main.dart
+++ lib/templates/lab5/main.dart
@@ -1,18 +1,197 @@
-// Use these dependencies for your classes
-import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:json_annotation/json_annotation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/material.dart';
+import 'package:education/templates/lab5/user.dart';
+import 'package:education/templates/lab5/post.dart';
+import 'package:education/templates/lab5/comment.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/cat': (context) => CatStatusCodePage(statusCode: ModalRoute.of(context)!.settings.arguments as String),
+        '/users': (context) => UsersPage(),
+        '/posts': (context) => PostsPage(),
+        '/comments': (context) => CommentsPage(),
+      },
+    );
+  }
+}
 
-// TODO add needed classes for Flutter APP
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-// TODO add generated route flutter app with undifined page with cat status code using api
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
 
-// TODO add putting argument in route navigation as parameter for generated page
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter status code',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            ElevatedButton(
+              child: Text('Go to Cat Page'),
+              onPressed: () {
+                String statusCode = _controller.text.isEmpty ? '404' : _controller.text;
+                Navigator.pushNamed(context, '/cat', arguments: statusCode);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Go to Users Page'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/users');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Go to Comments Page'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/comments');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Go to Posts Page'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/posts');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-// TODO use api with cat status codes
-// https://http.cat/[status_code]
+class CatStatusCodePage extends StatelessWidget {
+  final String statusCode;
 
+  const CatStatusCodePage({Key? key, required this.statusCode})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = 'https://http.cat/${statusCode.split('/').last}';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Status Code: ${statusCode.split('/').last}'),
+      ),
+      body: Center(
+        child: Image.network(imageUrl),
+      ),
+    );
+  }
+}
+
+class UsersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Users'),
+      ),
+      body: FutureBuilder(
+        future: fetchUsers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: (snapshot.data as List<User>).length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text((snapshot.data as List<User>)[index].name!),
+                  subtitle: Text((snapshot.data as List<User>)[index].email!),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+class CommentsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Comments'),
+      ),
+      body: FutureBuilder(
+        future: fetchComments(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: (snapshot.data as List<Comment>).length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text((snapshot.data as List<Comment>)[index].name),
+                  subtitle: Text((snapshot.data as List<Comment>)[index].body),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PostsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+      body: FutureBuilder(
+        future: fetchPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: (snapshot.data as List<Post>).length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text((snapshot.data as List<Post>)[index].title),
+                  subtitle: Text((snapshot.data as List<Post>)[index].body),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
