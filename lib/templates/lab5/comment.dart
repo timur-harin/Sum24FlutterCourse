54ed5f1de2fa631aa9bc5d6c2876340b04ce2249
Demetrius <thedioxider@mail.ru>
2024-06-19 22:42:04 +0300

    done: Lab 5 (incomplete)

--- lib/templates/lab5/comment.dart
+++ lib/templates/lab5/comment.dart
@@ -1,16 +1,36 @@
-// TODO add dependencies
-// TODO add comment.g.dart as part
+import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'comment.g.dart';
+
+@JsonSerializable()
 class Comment {
   // TODO task 2 to make this class for url http://jsonplaceholder.typicode.com/comments
+  final int postId;
+  final int id;
+  final String name;
+  final String email;
+  final String body;
 
-  factory Comment.fromJson(Map<String, dynamic> json) {}
+  Comment(
+      {required this.postId,
+      required this.id,
+      required this.name,
+      required this.email,
+      required this.body});
 
+  factory Comment.fromJson(Map<String, dynamic> json) =>
+      _$CommentFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CommentToJson(this);
   // Do not forget to run 'dart run build_runner build' to generate comment.g.dart
 }
 
-Future<List<Comment>> fetchComments() async {
-  // TODO task 2.2 to make this function for url http://jsonplaceholder.typicode.com/comments
-  // // Using fabric from class
-  return [];
+Future<List<Comment>> fetchComments({int start = 0, required int count}) async {
+  const url = 'http://jsonplaceholder.typicode.com/comments';
+  List<Comment> comments = [];
+  for (int i = start; i < start + count; ++i) {
+    comments.add(Comment.fromJson((await Dio().get('$url/$i')).data));
+  }
+  return comments;
 }
