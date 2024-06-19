71a97247ecb7cb225f08a4e6dde5293469e67e06
RamzeusInno <ramzikbosss@gmail.com>
2024-06-19 20:57:20 +0300

    lab5

--- lib/templates/lab5/post.dart
+++ lib/templates/lab5/post.dart
@@ -1,15 +1,28 @@
-// TODO add dependencies
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
 
+part 'post.g.dart';
+
+@JsonSerializable()
 class Post {
-  // TODO task 1 to make this class for url http://jsonplaceholder.typicode.com/posts
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
 
-  factory Post.fromJson(Map<String, dynamic> json) {}
+  Post({required this.userId, required this.id, required this.title, required this.body});
 
-  factory Post.toJson(Post post) {}
+  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+  Map<String, dynamic> toJson() => _$PostToJson(this);
 }
 
 Future<List<Post>> fetchPosts() async {
-  // TODO task 1.2 to make this function for url http://jsonplaceholder.typicode.com/posts
-  // // Using fabric from class
-  return [];
+  final response = await http.get('http://jsonplaceholder.typicode.com/posts' as Uri);
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((post) => Post.fromJson(post)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
 }
