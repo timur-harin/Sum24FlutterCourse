a84368518271a4e1c284b1ac87275eaee403895c
Леонид <macbook_leonid@MacBook-Pro-Leonid.local>
2024-06-19 19:10:25 +0300

    lab5 complete

--- lib/templates/lab5/main.dart
+++ lib/templates/lab5/main.dart
@@ -1,18 +1,224 @@
-// Use these dependencies for your classes
-import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:json_annotation/json_annotation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/material.dart';
+import 'comment.dart';
+import 'post.dart';
+import 'user.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/generated') {
+          final int statusCode = settings.arguments as int;
+          return MaterialPageRoute(
+            builder: (context) => GeneratedPage(statusCode: statusCode),
+          );
+        } else {
+          return MaterialPageRoute(
+            builder: (context) => UndefinedRoutePage(settings.name),
+          );
+        }
+      },
+      home: HomePage(),
+    );
+  }
+}
 
-// TODO add needed classes for Flutter APP
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-// TODO add generated route flutter app with undifined page with cat status code using api
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
 
-// TODO add putting argument in route navigation as parameter for generated page
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                final comments = await fetchComments();
+                showDialog(
+                  context: context,
+                  builder: (context) => SimpleDialog(
+                    title: Text('Comments'),
+                    children: comments.map((comment) {
+                      return ListTile(
+                        title: Text(comment.name),
+                        subtitle: Text(comment.body),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => SimpleDialog(
+                              title: Text(comment.name),
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(comment.body),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
+              child: Text('Show Comments'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final posts = await fetchPosts();
+                showDialog(
+                  context: context,
+                  builder: (context) => SimpleDialog(
+                    title: Text('Posts'),
+                    children: posts.map((post) {
+                      return ListTile(
+                        title: Text(post.title),
+                        subtitle: Text(post.body),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => SimpleDialog(
+                              title: Text(post.title),
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(post.body),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
+              child: Text('Show Posts'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final users = await fetchUsers();
+                showDialog(
+                  context: context,
+                  builder: (context) => SimpleDialog(
+                    title: Text('Users'),
+                    children: users.map((user) {
+                      return ListTile(
+                        title: Text(user.name ?? 'No Name'),
+                        subtitle: Text(user.email ?? 'No Email'),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => SimpleDialog(
+                              title: Text(user.name ?? 'No Name'),
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(user.email ?? 'No Email'),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
+              child: Text('Show Users'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Enter status code',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final statusCode = int.tryParse(_controller.text);
+                if (statusCode != null) {
+                  Navigator.of(context).pushNamed('/generated', arguments: statusCode);
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Invalid Input'),
+                      content: Text('Please enter a valid number.'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: Text('Show Cat Status'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-// TODO use api with cat status codes
-// https://http.cat/[status_code]
+class GeneratedPage extends StatelessWidget {
+  final int statusCode;
 
+  GeneratedPage({required this.statusCode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Image.network('https://http.cat/$statusCode'),
+      ),
+    );
+  }
+}
+
+class UndefinedRoutePage extends StatelessWidget {
+  final String? routeName;
+
+  UndefinedRoutePage(this.routeName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Undefined Route'),
+      ),
+      body: Center(
+        child: Image.network('https://http.cat/404'),
+      ),
+    );
+  }
+}
--- lib/templates/lab5/post.dart
+++ lib/templates/lab5/post.dart
@@ -1,15 +1,45 @@
 // TODO add dependencies
+// TODO task 1 to make this class for url http://jsonplaceholder.typicode.com/posts
+// TODO task 1.2 to make this function for url http://jsonplaceholder.typicode.com/posts
+// Using fabric from class
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Post {
-  // TODO task 1 to make this class for url http://jsonplaceholder.typicode.com/posts
+  final int id;
+  final String title;
+  final String body;
+  final int userId;
 
-  factory Post.fromJson(Map<String, dynamic> json) {}
+  Post({required this.id, required this.title, required this.body, required this.userId});
 
-  factory Post.toJson(Post post) {}
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      userId: json['userId'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+      'userId': userId,
+    };
+  }
 }
 
 Future<List<Post>> fetchPosts() async {
-  // TODO task 1.2 to make this function for url http://jsonplaceholder.typicode.com/posts
-  // // Using fabric from class
-  return [];
+  final response = await http.get(Uri.parse('http://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> postsJson = json.decode(response.body);
+    return postsJson.map((json) => Post.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
 }
