a3cf84b4e95ae4a822445ac7c8527b1aa1741a76
Mikhail Trifonov <m.trifonov@innopolis.university>
2024-06-19 20:57:38 +0300

    Lab5 solutions

--- lib/templates/lab5/post.dart
+++ lib/templates/lab5/post.dart
@@ -1,15 +1,45 @@
-// TODO add dependencies
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Post {
-  // TODO task 1 to make this class for url http://jsonplaceholder.typicode.com/posts
+  final int id;
+  final String title;
+  final String body;
+  final int userId;
 
-  factory Post.fromJson(Map<String, dynamic> json) {}
+  Post(
+      {required this.id,
+      required this.title,
+      required this.body,
+      required this.userId});
 
-  factory Post.toJson(Post post) {}
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      userId: json['userId'],
+    );
+  }
+
+  factory Post.toJson(Post post) {
+    return Post(
+      id: post.id,
+      title: post.title,
+      body: post.body,
+      userId: post.userId,
+    );
+  }
 }
 
 Future<List<Post>> fetchPosts() async {
-  // TODO task 1.2 to make this function for url http://jsonplaceholder.typicode.com/posts
-  // // Using fabric from class
-  return [];
+  final response =
+      await http.get(Uri.parse('http://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> jsonResponse = jsonDecode(response.body);
+    return jsonResponse.map((data) => Post.fromJson(data)).toList();
+  } else {
+    throw Exception('No posts, Sir');
+  }
 }
