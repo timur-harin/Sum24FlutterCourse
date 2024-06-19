5d9745c157b46b40fc8c040a6e316b1c7cd3f936
GG_Host <gd.host@yandex.ru>
2024-06-19 19:28:15 +0300

    lab 5 antipov dmitrii

--- lib/templates/lab5/main.dart
+++ lib/templates/lab5/main.dart
@@ -1,18 +1,151 @@
-// Use these dependencies for your classes
 import 'dart:convert';
-import 'package:http/http.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:json_annotation/json_annotation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() {}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HTTP Cat Status Codes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      onGenerateRoute: RouteGenerator.generateRoute,
+      initialRoute: '/',
+    );
+  }
+}
 
-// TODO add needed classes for Flutter APP
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HTTP Cat Status Codes'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/status',
+                  arguments: 404,
+                );
+              },
+              child: const Text('Show 404 Cat'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/status',
+                  arguments: 200,
+                );
+              },
+              child: const Text('Show 200 Cat'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-// TODO add generated route flutter app with undifined page with cat status code using api
+class StatusPage extends StatelessWidget {
+  final int statusCode;
 
-// TODO add putting argument in route navigation as parameter for generated page
+  StatusPage({required this.statusCode});
 
-// TODO use api with cat status codes
-// https://http.cat/[status_code]
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Status $statusCode'),
+      ),
+      body: Center(
+        child: Image.network(
+          'https://http.cat/$statusCode',
+          errorBuilder:
+              (BuildContext context, Object exception, StackTrace? stackTrace) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.error,
+                  color: Colors.red,
+                  size: 64.0,
+                ),
+                const SizedBox(height: 16.0),
+                Text(
+                  'Failed to load image for status $statusCode',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
 
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/status':
+        if (settings.arguments is int) {
+          final int statusCode = settings.arguments as int;
+          return MaterialPageRoute(
+            builder: (_) => StatusPage(statusCode: statusCode),
+          );
+        }
+        return _errorRoute();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://http.cat/404',
+            errorBuilder: (BuildContext context, Object exception,
+                StackTrace? stackTrace) {
+              return const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.error,
+                    color: Colors.red,
+                    size: 64.0,
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Failed to load image for status 404',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
