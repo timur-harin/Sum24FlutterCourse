f3da04830adf78e498c3bcc48ff457c080d79774
Valeria Neganova <v.neganova@innopolis.university>
2024-06-19 18:08:18 +0300

    assigment

--- lib/templates/middleAssignment/main.dart
+++ lib/templates/middleAssignment/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:education/templates/middleAssignment/screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MiddleAssigmentApp());
+  runApp(const ProviderScope(child: MiddleAssigmentApp()) );
 }
 
 class MiddleAssigmentApp extends StatelessWidget {
@@ -11,11 +16,36 @@ class MiddleAssigmentApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Middle Assigment',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0x2D3037FF),
+        textTheme: const TextTheme(bodyText1: TextStyle(color: Colors.white)),
       ),
-      // TODO - complete assignment
-      home: null,
+      
+      home: const HomeScreen(),
     );
   }
+}
+
+class ShowerSession {
+  final DateTime dateTime;
+  final int duration;
+  final String feedback;
+
+  ShowerSession({
+    required this.dateTime,
+    required this.duration,
+    required this.feedback,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'dateTime': dateTime.toIso8601String(),
+        'duration': duration,
+        'feedback': feedback,
+      };
+
+  factory ShowerSession.fromJson(Map<String, dynamic> json) =>  ShowerSession(
+    dateTime: DateTime.parse(json['dateTime']),
+    duration: json['duration'],
+    feedback: json['feedback'],
+  );
 }
---
+++ lib/templates/middleAssignment/screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+class HomeScreen extends ConsumerWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch)
+}
