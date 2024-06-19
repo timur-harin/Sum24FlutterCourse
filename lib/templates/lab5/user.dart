4782c6da6e7f5a1dea4a42074b97dd3807a60859
samer spc <samerspc@MacBook-Air-samer.local>
2024-06-19 21:40:55 +0300

    lab 5 samat iakupov

--- lib/templates/lab5/user.dart
+++ lib/templates/lab5/user.dart
@@ -1,15 +1,92 @@
-// TODO add dependencies
-// TODO add user.g.dart as part
-// TODO add user.freezed.dart as part
+import 'dart:convert';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:http/http.dart' as http;
+part 'user.freezed.dart';
+part 'user.g.dart';
 
 @freezed
 class User with _$User {
-  // TODO task 3 to make this class for url http://jsonplaceholder.typicode.com/users/
+  factory User({
+    required int id,
+    required String name,
+    required String username,
+    required String email,
+    required Address address,
+    required String phone,
+    required String website,
+    required Company company,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
 
 Future<List<User>> fetchUsers() async {
-  // TODO task 3.2 to make this function for url http://jsonplaceholder.typicode.com/users/ 
-  // Using fabric from class
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
-  return [];
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((user) => User.fromJson(user as Map<String, dynamic>)).toList();
+  } else {
+    throw Exception('No users, Sir');
+  }
+}
+
+@JsonSerializable()
+class Address {
+  final String street;
+  final String suite;
+  final String city;
+  final String zipcode;
+  final Geo geo;
+
+  Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+    required this.geo,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return _$AddressFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$AddressToJson(this);
+}
+
+@JsonSerializable()
+class Geo {
+  final String lat;
+  final String lng;
+
+  Geo({
+    required this.lat,
+    required this.lng,
+  });
+
+  factory Geo.fromJson(Map<String, dynamic> json) {
+    return _$GeoFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$GeoToJson(this);
+}
+
+@JsonSerializable()
+class Company {
+    final String name;
+    final String catchPhrase;
+    final String bs;
+
+  Company({
+    required this.name,
+    required this.catchPhrase,
+    required this.bs,
+  });
+
+  factory Company.fromJson(Map<String, dynamic> json) {
+    return _$CompanyFromJson(json);
+  }
+    
+  Map<String, dynamic> toJson() => _$CompanyToJson(this); 
 }
