ebae8ea4d39ac1321f5f1097e1ffbd35219518ef
GG_Host <gd.host@yandex.ru>
2024-06-19 19:56:21 +0300

    complited

--- lib/templates/lab5/main.dart
+++ lib/templates/lab5/main.dart
@@ -1,97 +1,184 @@
-import 'dart:convert';
+import 'package:education/templates/lab5/comment.dart';
+import 'package:education/templates/lab5/post.dart';
+import 'package:education/templates/lab5/user.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:json_annotation/json_annotation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApplication());
 }
 
-class MyApp extends StatelessWidget {
+class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HTTP Cat Status Codes',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       onGenerateRoute: RouteGenerator.generateRoute,
-      initialRoute: '/',
+      home: MainPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HTTP Cat Status Codes'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  '/status',
-                  arguments: 404,
-                );
-              },
-              child: const Text('Show 404 Cat'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  '/status',
-                  arguments: 200,
-                );
-              },
-              child: const Text('Show 200 Cat'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  _MainPageState createState() => _MainPageState();
 }
 
-class StatusPage extends StatelessWidget {
-  final int statusCode;
-
-  StatusPage({required this.statusCode});
+class _MainPageState extends State<MainPage> {
+  final TextEditingController _inputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Status $statusCode'),
+        title: Text('Home'),
       ),
       body: Center(
-        child: Image.network(
-          'https://http.cat/$statusCode',
-          errorBuilder:
-              (BuildContext context, Object exception, StackTrace? stackTrace) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.error,
-                  color: Colors.red,
-                  size: 64.0,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () async {
+                  final posts = await fetchPosts();
+                  showDialog(
+                    context: context,
+                    builder: (context) => SimpleDialog(
+                      title: Text('Posts'),
+                      children: posts.map((post) {
+                        return ListTile(
+                          title: Text(post.title),
+                          subtitle: Text(post.body),
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => SimpleDialog(
+                                title: Text(
+                                  post.title,
+                                  style: const TextStyle(
+                                      color: Color.fromRGBO(182, 10, 10, 1)),
+                                ),
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Text(post.body),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+                child: Text('View Posts'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final comments = await fetchComments();
+                  showDialog(
+                    context: context,
+                    builder: (context) => SimpleDialog(
+                      title: Text('Comments'),
+                      children: comments.map((comment) {
+                        return ListTile(
+                          title: Text(comment.name),
+                          subtitle: Text(comment.body),
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => SimpleDialog(
+                                title: Text(comment.name),
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Text(comment.body),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+                child: Text('Comments'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final users = await fetchUsers();
+                  showDialog(
+                    context: context,
+                    builder: (context) => SimpleDialog(
+                      title: Text('Users'),
+                      children: users.map((user) {
+                        return ListTile(
+                          title: Text(user.name),
+                          subtitle: Text(user.email),
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => SimpleDialog(
+                                title: Text(user.name),
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(user.email),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+                child: Text('Users'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _inputController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Enter status code',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                const SizedBox(height: 16.0),
-                Text(
-                  'Failed to load image for status $statusCode',
-                  style: const TextStyle(fontSize: 16.0),
-                ),
-              ],
-            );
-          },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final statusCode = int.tryParse(_inputController.text);
+                  if (statusCode != null) {
+                    Navigator.of(context)
+                        .pushNamed('/status', arguments: statusCode);
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text('Invalid Input'),
+                        content: Text('Please enter a valid number'),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+                child: Text('Show Cat Image'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -101,8 +188,6 @@ class StatusPage extends StatelessWidget {
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => HomePage());
       case '/status':
         if (settings.arguments is int) {
           final int statusCode = settings.arguments as int;
@@ -123,28 +208,24 @@ class RouteGenerator {
           title: Text('Error'),
         ),
         body: Center(
-          child: Image.network(
-            'https://http.cat/404',
-            errorBuilder: (BuildContext context, Object exception,
-                StackTrace? stackTrace) {
-              return const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.error,
-                    color: Colors.red,
-                    size: 64.0,
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'Failed to load image for status 404',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              );
-            },
-          ),
+          child: Image.network('https://http.cat/404'),
         ),
+      ),
+    );
+  }
+}
+
+class StatusPage extends StatelessWidget {
+  final int statusCode;
+
+  StatusPage({required this.statusCode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Image.network('https://http.cat/$statusCode'),
       ),
     );
   }
