20835b3d1215458481726ab97e0056458486c4f7
Dmitrii-Bargan <d.bargan@innopolis.university>
2024-06-26 10:15:00 +0300

    Lab 6: Task 1 done

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
 bool isValidEmail(String email) {
-  // TODO add your validation logic here
-  return false;
+  // // TODO add your validation logic here
+  // // return false;
+  final emailRegex =
+      RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+  return emailRegex.hasMatch(email);
 }
 
 void main() {
@@ -11,8 +14,15 @@ void main() {
       expect(isValidEmail('test@example.com'), true);
     });
 
-    //  TODO add more test cases for invalid emails
+    // // TODO add more test cases for invalid emails
+    test('Invalid email returns false', () {
+      expect(isValidEmail('test@example@somewhere:there'), false);
+    });
+    test('Another invalid email returns false', () {
+      expect(isValidEmail('This is definitely not an email'), false);
+    });
   });
 
   // Test using flutter test test/unit_test.dart
+  // Done. All tests pass!
 }
