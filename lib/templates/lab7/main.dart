da550cd714510d22210bb2168ca15cb52c77849f
StasiaNenastie <malakhova5338885@mail.ru>
2024-06-26 18:53:51 +0300

    TestTask2

--- lib/templates/lab7/main.dart
+++ lib/templates/lab7/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:collection';
+
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   runApp(
---
+++ test/widgetTest.dart
@@ -0,0 +1,12 @@
+import 'package:education/templates/lab7/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Check widget',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: MyHomePage(),));
+
+    expect(find.text('Hello World!'), findsAny);
+  });
+}
