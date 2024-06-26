6acd02c3ff9accda101cecc5f4d7c3c6864b7e95
Otkisaev <polarevia@bk.ru>
2024-06-26 19:00:31 +0300

    1

--- test/test.dart
+++ test/test.dart
@@ -1,12 +1,24 @@
 
+import 'package:education/templates/lab4/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-  group('Empty Validation', () {
-    test('Empty string returns true', () {
-      expect(true, true);
-    });
-  });
+  testWidgets('MyHomePage displays "Hello World!"', (WidgetTester tester) async {
+    // Arrange
+    await tester.pumpWidget(
+       ProviderScope(
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
 
+    // Act
+    await tester.pump(); // Rebuild the widget after the initial build
+
+    // Assert
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
 }
