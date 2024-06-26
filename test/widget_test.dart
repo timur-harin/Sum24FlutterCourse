9bda75fdef04075d340299eb49b424696f871420
lanebo1 <k.efimovich@innopolis.university>
2024-06-26 18:08:14 +0300

    Test Task2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:education/templates/lab7/main.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('MyHomePage has a Text widget', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyHomePage());
+    await tester.pumpWidget(MaterialApp(home: const MyHomePage()));
     expect(find.text('Hello World!'), findsOneWidget);
   });
 }
