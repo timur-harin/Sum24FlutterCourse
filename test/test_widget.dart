a7aa373cbde2121bdaf7910c3425785dea8202e3
FunnyFox <tskhe_daniil@mail.ru>
2024-06-26 21:13:59 +0300

    work plzzzzzzzzzz

--- test/test_widget.dart
+++ test/test_widget.dart
@@ -1,9 +1,11 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:education/templates/lab7/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('MyHomePage has a Text widget', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyHomePage());
-    expect(find.text('Hello Flutter Developers!'), findsOneWidget);
+  testWidgets('MyHomePage has a Text widget with "Hello World!"', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MaterialApp(home: MyHomePage())));
+    expect(find.text('Hello World!'), findsOneWidget);
   });
 }
