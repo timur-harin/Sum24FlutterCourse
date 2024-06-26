ef6746100233e1836b536bb7183fd4a2191ecb1d
Dmitrii-Bargan <d.bargan@innopolis.university>
2024-06-26 11:01:39 +0300

    Lab 6: Task 2 done

--- test/profile_test.dart
+++ test/profile_test.dart
@@ -1,6 +1,7 @@
 import 'package:education/templates/lab6/card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 void main() {
   testWidgets('ProfileCard displays correct information',
@@ -11,11 +12,29 @@ void main() {
     const description = 'Software Developer';
 
     // Act
-    // TODO add test using pumpWidget
+    // // TODO add test using pumpWidget
+    // https://pub.dev/packages/network_image_mock
+    await mockNetworkImagesFor(() => tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: ProfileCard(
+                imageUrl: imageUrl,
+                name: name,
+                description: description,
+              ),
+            ),
+          ),
+        ));
 
     // Assert
-    // TODO add assertions using expect and findsOneWidget
+    // // TODO add assertions using expect and findsOneWidget
+    expect(find.byType(ProfileCard), findsOneWidget);
+    expect(find.text(name), findsOneWidget);
+    expect(find.text(description), findsOneWidget);
+    final avatar = tester.widget<CircleAvatar>(find.byType(CircleAvatar));
+    expect((avatar.backgroundImage as NetworkImage).url, equals(imageUrl));
 
-    // Test using flutter test test/custom_button_test.dart
+    // Test using flutter test test/profile_test.dart
+    // Done. All tests pass!
   });
 }
