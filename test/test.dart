f90e269b72e668ae19d8117b1f1c7e0405bd3a9e
SerggAidd <56043437+SerggAidd@users.noreply.github.com>
2024-06-26 21:17:58 +0300

    Lab 7

--- test/test.dart
+++ test/test.dart
@@ -1,11 +1,15 @@
+import 'package:education/templates/lab7/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-  group('Empty Validation', () {
-    test('Empty string returns true', () {
-      expect(true, true);
-    });
+  testWidgets('App starts and displays Hello World', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: MyHomePage(),
+      ),
+    );
+    expect(find.text('Hello World!'), findsOneWidget);
   });
-
 }
