180565a1950af47d780735e7f66da277bce1b597
Kirill <mailkirill17@gmail.com>
2024-06-26 18:18:22 +0300

    CICD commit

---
+++ test/lab7_test.dart
@@ -0,0 +1,13 @@
+import 'package:education/templates/lab4/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('ProfileCard displays correct information',
+          (WidgetTester tester) async {
+
+    await tester.pumpWidget(ProviderScope(child: MaterialApp(home: MyHomePage())));
+    expect(find.byType(Text), findsAny);
+  });
+}
