07bb90374027987b919610ddd1d57cbe84dba3e8
PrizrakZamkov <prizrakzamkov@gmail.com>
2024-06-26 13:54:09 +0300

    lab6

--- test/profile_test.dart
+++ test/profile_test.dart
@@ -1,21 +1,37 @@
 import 'package:education/templates/lab6/card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 void main() {
   testWidgets('ProfileCard displays correct information',
       (WidgetTester tester) async {
-    // Arrange
-    const imageUrl = 'https://example.com/image.jpg';
-    const name = 'John Doe';
-    const description = 'Software Developer';
+
+        const imageUrl =
+            'https://sneg.top/uploads/posts/2023-06/1687935518_sneg-top-p-kietaka-ayanokodzhi-avatarka-pinterest-44.png';
+        const name = 'Something name';
+        const description = 'Software Developer';
+
+        await mockNetworkImagesFor(() async => await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: ProfileCard(
+              imageUrl: imageUrl,
+              name: name,
+              description: description,
+            ),
+          ),
+        )));
 
     // Act
     // TODO add test using pumpWidget
 
     // Assert
     // TODO add assertions using expect and findsOneWidget
-
+        expect(find.byType(ProfileCard), findsOneWidget);
+        expect(find.byType(CircleAvatar), findsOneWidget);
+        expect(find.byType(Text), findsNWidgets(2));
+        expect(find.text(name), findsOneWidget);
+        expect(find.text(description), findsOneWidget);
     // Test using flutter test test/custom_button_test.dart
   });
 }
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 bool isValidEmail(String email) {
-  // TODO add your validation logic here
-  return false;
+  if (email.length < 5 || email.length > 40) {
+    return false;
+  }
+  if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email)) {
+    return false;
+  }
+  return true;
 }
 
 void main() {
@@ -10,8 +16,27 @@ void main() {
     test('Valid email returns true', () {
       expect(isValidEmail('test@example.com'), true);
     });
-
-    //  TODO add more test cases for invalid emails
+    test(
+      'Empty Email', () {
+        expect(isValidEmail(''), false);
+      }
+    );
+    
+    test('Email without @', () {
+      expect(isValidEmail('testexample.com'), false);
+    });
+    
+    test('Email without .com', () {
+      expect(isValidEmail('test@example'), false);
+    });
+    
+    test('Email with @ and .com but invalid format', () {
+      expect(isValidEmail('test@example..com'), false);
+    });
+    
+    test('Email with @ and .com but too long', () {
+      expect(isValidEmail('test@exampleexrkjgpwrpmwphnphrnpnhpwnrphamplenamerobert.com'), false);
+    });
   });
 
   // Test using flutter test test/unit_test.dart
