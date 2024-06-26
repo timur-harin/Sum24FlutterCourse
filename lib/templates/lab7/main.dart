999254fd33bfa54a962e7125dc74fac84a1b6144
SerggAidd <56043437+SerggAidd@users.noreply.github.com>
2024-06-26 21:00:01 +0300

    Lab 7

--- lib/templates/lab7/main.dart
+++ lib/templates/lab7/main.dart
@@ -4,35 +4,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
   runApp(
     const ProviderScope(
-      child: MyApp(),
-    ),
+        child: MaterialApp(
+          home: MyHomePage(),
+        )),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
-  }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyWidgetState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Hello World!'),
+        child: Container(
+          child: Text('Hello World!'),
+        ),
       ),
     );
   }
