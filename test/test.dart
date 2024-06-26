136faa11f83db40689b238043f72ec689b7df50e
Leropsis <ksoksero@yandex.ru>
2024-06-26 18:41:58 +0300

    Lab 7

--- test/test.dart
+++ test/test.dart
@@ -1,12 +1,11 @@
-
+import 'package:education/templates/lab7/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
-  group('Empty Validation', () {
-    test('Empty string returns true', () {
-      expect(true, true);
-    });
+  testWidgets('Test for task 2', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: MyHomePage()));
+    expect(find.text("Hello World!"), findsOneWidget);
   });
-
 }
