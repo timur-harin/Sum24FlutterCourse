76b31cbed53d6c026dd809e747d25fce6ee98f44
thallars <arszemlyanikin@gmail.com>
2024-06-26 18:45:38 +0300

    lab7 wip

--- test/test.dart
+++ test/test.dart
@@ -1,12 +1,19 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:education/templates/lab7/main.dart';
 
 void main() {
-  group('Empty Validation', () {
-    test('Empty string returns true', () {
-      expect(true, true);
-    });
-  });
+  testWidgets('MyHomePage displays correct information', 
+  (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: MyHomePage(),
+        ),
+      ),
+    );
 
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
 }
