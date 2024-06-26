65a0814247042cc3424bd4e74d151212185bdf43
Timur Harin <timurx404@gmail.com>
2024-06-26 09:32:47 +0300

    add github actions

---
+++ test/test.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  group('Empty Validation', () {
+    test('Valid email returns true', () {
+      expect(false, true);
+    });
+  });
+
+}
