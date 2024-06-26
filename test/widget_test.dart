dbe3507c759ab31740f831b9c6dcb11279bdaf7d
Resent <remal11151@gmail.com>
2024-06-26 18:38:02 +0300

    pubspec fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:education/templates/lab7/main.dart';
 
 void main() {
-  testWidgets('MyHomePage has a Text widget that displays "Hello World!"', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('MyHomePage has a Text widget that displays "Hello World!"',
+      (WidgetTester tester) async {
     await tester.pumpWidget(const MyHomePage());
 
-    // Verify that our Text widget displays "Hello World!".
     expect(find.text('Hello World!'), findsOneWidget);
   });
 }
