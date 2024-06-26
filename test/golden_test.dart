462bb2eeb9b05b19db59015e94624d7ac856ac89
Kirill <mailkirill17@gmail.com>
2024-06-26 10:50:21 +0300

    Completed lab 6

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,14 +1,30 @@
 import 'package:education/templates/lab6/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   testGoldens('LoginScreen golden test', (WidgetTester tester) async {
     final builder = GoldenBuilder.grid(
       columns: 1,
       widthToHeightRatio: 0.5,
-    )..addScenario('Login Screen', MaterialApp(home: LoginScreen()));
+    )..addScenario('Login Screen', MaterialApp(
+        locale: const Locale("ru"),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalizations.delegate
+          // Add your generated delegate here
+        ],
+        supportedLocales: const [
+          Locale('en'),
+          Locale('ru'),
+        ],
+        home: LoginScreen())
+    );
 
     await tester.pumpWidgetBuilder(
       builder.build(),
