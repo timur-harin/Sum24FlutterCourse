4b3c0ade441f3416eeef73dca923acf6ea98d13a
lanebo1 <k.efimovich@innopolis.university>
2024-06-26 18:00:50 +0300

    Test Task2

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:education/templates/lab7/main.dart';
+
+void main() {
+  testWidgets('MyHomePage has a Text widget', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyHomePage());
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
