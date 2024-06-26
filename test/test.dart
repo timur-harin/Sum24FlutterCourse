9cb3af15c1a328869c3154421c7897266ed6bd54
Otkisaev <polarevia@bk.ru>
2024-06-26 19:27:57 +0300

    3

--- test/test.dart
+++ test/test.dart
@@ -1,22 +1,17 @@
 
-import 'package:education/templates/lab4/main.dart';
+import 'package:education/templates/lab7/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('MyHomePage displays "Hello World!"', (WidgetTester tester) async {
-    // Arrange
     await tester.pumpWidget(
-       ProviderScope(
-        child: MaterialApp(
+       MaterialApp(
           home: MyHomePage(),
         ),
-      ),
+  
     );
-
-
-    await tester.pump();
 
     expect(find.text('Hello World!'), findsOneWidget);
   });
