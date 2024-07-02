f68dd922f9f61ddd00060e0e54258479ebd35242
Otkisaev <polarevia@bk.ru>
2024-07-02 13:48:00 +0300

    lab1

--- lib/templates/lab1/lab1.dart
+++ lib/templates/lab1/lab1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const Lab1());
 
@@ -8,13 +9,13 @@ class Lab1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LAb1HomePage(),
+      home: Lab1HomePage(),
     );
   }
 }
 
-class LAb1HomePage extends StatelessWidget {
-  const LAb1HomePage({super.key});
+class Lab1HomePage extends StatelessWidget {
+  const Lab1HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -57,32 +58,71 @@ class LAb1HomePage extends StatelessWidget {
   }
 
   Widget exercise1() {
-    return const FlutterLogo(
-      size: 100,
+    return const Text(
+      "Hello, Flutter!",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.lightBlue,
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 
   Widget exercise2() {
-    return const FlutterLogo(
+    return const Icon(
+      Icons.home,
       size: 100,
+      color: Colors.green,
     );
   }
 
   Widget exercise3() {
-    return const FlutterLogo(
-      size: 100,
+    return const Image(
+      width: 200,
+      height: 200,
+      image: NetworkImage('https://http.dog/101.jpg'),
+      fit: BoxFit.cover,
     );
   }
 
+  void printPressed() {
+    print("User just Pressed Me");
+  }
+
   Widget exercise4() {
-    return const FlutterLogo(
-      size: 100,
+    return TextButton(
+      style: ButtonStyle( 
+        backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
+      ),
+      onPressed: printPressed,
+      child: const Text('I am red , CLICK ON ME!'),
     );
   }
 
   Widget exercise5() {
-    return const FlutterLogo(
-      size: 100,
+    return Column(
+      children: [
+        Container(
+          color: Color.fromARGB(255, 24, 236, 31),
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(10),
+          child: const Text(
+            "This is a Text in a Container!",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+
+        Container(
+          color: Colors.green,
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(10),
+          child: const Icon(
+            Icons.home,
+            size: 50,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
